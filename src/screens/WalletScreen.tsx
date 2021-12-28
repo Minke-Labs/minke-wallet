7cfeaf0320750a31770fb26f2a44f14fc0b422f8
avelar <fortunato.avelar@gmail.com>
2021-12-28 18:02:14 -0300

    init home UI

--- src/screens/WalletScreen.tsx
+++ src/screens/WalletScreen.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback } from 'react';
-import { Dimensions, StyleSheet, Text, View } from 'react-native';
-import { Appbar, Button, Card, Dialog, Portal, Snackbar } from 'react-native-paper';
+import { Text, View, Image, TouchableOpacity, SafeAreaView, ScrollView } from 'react-native';
+import { Appbar, Button, Card, Dialog, Portal, Snackbar, useTheme } from 'react-native-paper';
 import { globalWalletState } from '../stores/WalletStore';
 import AppLoading from 'expo-app-loading';
 import { useState } from '@hookstate/core';
@@ -12,11 +12,15 @@ import { isNaN } from 'lodash';
 import { commify, formatEther, formatUnits } from 'ethers/lib/utils';
 import * as Clipboard from 'expo-clipboard';
 import QRCode from 'react-native-qrcode-svg';
+import { MaterialIcons } from '@expo/vector-icons';
+import styles from './home/styles';
+import avatar from './home/avatar-test.png';
 
 export function WalletScreen({ navigation }: NativeStackScreenProps<RootStackParamList>) {
 	const state = useState(globalWalletState());
 	const snackbarVisible = useState(false);
 	const dialogVisible = useState(false);
+	const { colors } = useTheme();
 
 	const transferTo = useState({ to: '', amount: '' });
 	if (state.promised) return <AppLoading />;
@@ -45,13 +49,129 @@ export function WalletScreen({ navigation }: NativeStackScreenProps<RootStackPar
 	};
 
 	return (
-		<View style={{ flex: 1 }}>
+		<View style={styles.container}>
 			<Appbar.Header>
 				<Appbar.Content title="Minke Wallet" />
 			</Appbar.Header>
-			<Card style={{ padding: 20 }}>
+
+			<View style={styles.paddingContent}>
+				<Card style={styles.card}>
+					<View style={styles.cardTopContent}>
+						<View>
+							<Text style={styles.cardLabel}>Your total assets</Text>
+							<Text style={styles.cardBalance}>${commify(state.value.balance?.usd || '')}</Text>
+						</View>
+						<View>
+							<Image source={avatar} style={styles.avatar} />
+						</View>
+					</View>
+					<View style={styles.cardBottomContent}>
+						<TouchableOpacity onPress={onTransfer} style={(styles.cardActionButton, styles.cardDivisor)}>
+							<MaterialIcons
+								name="add-circle-outline"
+								size={20}
+								color={colors.primary}
+								style={styles.cardButtonIcon}
+							/>
+							<Text>Add funds</Text>
+						</TouchableOpacity>
+						<TouchableOpacity onPress={onTransfer} style={styles.cardActionButton}>
+							<MaterialIcons
+								name="arrow-circle-up"
+								size={20}
+								color={colors.primary}
+								style={styles.cardButtonIcon}
+							/>
+							<Text>Send</Text>
+						</TouchableOpacity>
+					</View>
+				</Card>
+			</View>
+
+			<SafeAreaView>
+				<ScrollView
+					style={styles.scrollviewHorizontal}
+					horizontal={true}
+					showsVerticalScrollIndicator={false}
+					showsHorizontalScrollIndicator={false}
+				>
+					<View style={styles.scrollviewHorizontalContent}>
+						<Card style={styles.cardScroll}>
+							<TouchableOpacity onPress={onTransfer} style={styles.cardActionButton}>
+								<MaterialIcons
+									name="compare-arrows"
+									size={20}
+									color={colors.primary}
+									style={styles.cardButtonIcon}
+								/>
+								<Text>Exchange</Text>
+							</TouchableOpacity>
+						</Card>
+						<Card style={styles.cardScroll}>
+							<TouchableOpacity onPress={onTransfer} style={styles.cardActionButton}>
+								<MaterialIcons
+									name="arrow-circle-down"
+									size={20}
+									color={colors.primary}
+									style={styles.cardButtonIcon}
+								/>
+								<Text>Receive</Text>
+							</TouchableOpacity>
+						</Card>
+						<Card style={styles.cardScroll}>
+							<TouchableOpacity onPress={onTransfer} style={styles.cardActionButton}>
+								<MaterialIcons
+									name="content-copy"
+									size={20}
+									color={colors.primary}
+									style={styles.cardButtonIcon}
+								/>
+								<Text>Copy address</Text>
+							</TouchableOpacity>
+						</Card>
+						<Card style={styles.cardScroll}>
+							<TouchableOpacity onPress={onTransfer} style={styles.cardActionButton}>
+								<MaterialIcons
+									name="add"
+									size={20}
+									color={colors.primary}
+									style={styles.cardButtonIcon}
+								/>
+								<Text>New wallet</Text>
+							</TouchableOpacity>
+						</Card>
+						<Card style={styles.cardScroll}>
+							<TouchableOpacity onPress={onTransfer} style={styles.cardActionButton}>
+								<MaterialIcons
+									name="person-outline"
+									size={20}
+									color={colors.primary}
+									style={styles.cardButtonIcon}
+								/>
+								<Text>Switch accounts</Text>
+							</TouchableOpacity>
+						</Card>
+					</View>
+				</ScrollView>
+			</SafeAreaView>
+
+			<View style={styles.paddingContent}>
+				<Button style={{ marginBottom: 5 }} mode={'contained'} onPress={onTransfer}>
+					Transfer
+				</Button>
+				<Button style={{ marginBottom: 5 }} color={'red'} onPress={onDeleteWallet}>
+					Delete
+				</Button>
+
 				<Text style={{ marginBottom: 5 }}>{state.value.wallet?.address}</Text>
-				<View style={{ marginBottom: 5, flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+				<View
+					style={{
+						marginBottom: 5,
+						flexDirection: 'row',
+						alignItems: 'center',
+						justifyContent: 'center'
+					}}
+				>
 					<Button onPress={onCopyToClipboard}>Copy to Clipboard</Button>
 					<Button onPress={onShareAddress}>Share Address</Button>
 					<Portal>
@@ -64,10 +184,10 @@ export function WalletScreen({ navigation }: NativeStackScreenProps<RootStackPar
 						</Dialog>
 					</Portal>
 				</View>
+
 				<Text>
 					Balance ETH: {state.value.balance?.eth ? formatEther(state.value.balance.eth as BigNumberish) : ''}
 				</Text>
-				<Text style={{ marginBottom: 5 }}>Balance USD: {commify(state.value.balance?.usd || '')}</Text>
 
 				<View style={{ padding: 10 }}>
 					<Text>Tokens:</Text>
@@ -77,16 +197,11 @@ export function WalletScreen({ navigation }: NativeStackScreenProps<RootStackPar
 					</Text>
 				</View>
 				{/*        <TextInput label={'Transfer To'} value={transferTo.value.to}
-                           onChangeText={address => transferTo.to.set(address)}/>
-                <TextInput keyboardType={'number-pad'} label={'Amount'} value={transferTo.amount.value}
-                           onChangeText={onAmountChange}/>*/}
-				<Button style={{ marginBottom: 5 }} mode={'contained'} onPress={onTransfer}>
-					Transfer
-				</Button>
-				<Button style={{ marginBottom: 5 }} color={'red'} onPress={onDeleteWallet}>
-					Delete
-				</Button>
-			</Card>
+																onChangeText={address => transferTo.to.set(address)}/>
+											<TextInput keyboardType={'number-pad'} label={'Amount'} value={transferTo.amount.value}
+																onChangeText={onAmountChange}/>*/}
+			</View>
+
 			<Snackbar onDismiss={() => snackbarVisible.set(false)} visible={snackbarVisible.value}>
 				Address Copied
 			</Snackbar>
---
+++ src/screens/home/styles.tsx
@@ -0,0 +1,81 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#F2EAE1'
+	},
+	paddingContent: {
+		paddingTop: 24,
+		paddingLeft: 24,
+		paddingRight: 24,
+		paddingBottom: 16
+	},
+	card: {
+		backgroundColor: 'rgba(255, 255, 255, 0.8)',
+		borderRadius: 24,
+		overflow: 'hidden',
+		marginBottom: 16
+	},
+	cardTopContent: {
+		padding: 24,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center'
+	},
+	cardLabel: {
+		color: 'rgba(78, 94, 111, 1)',
+		fontSize: 14,
+		marginBottom: 8
+	},
+	cardBalance: {
+		color: 'rgba(10, 32, 54, 1)',
+		fontSize: 36
+	},
+	avatar: {
+		width: 48,
+		height: 48,
+		borderColor: 'rgba(243, 247, 255, 1)',
+		borderRadius: 50,
+		borderWidth: 4
+	},
+	cardBottomContent: {
+		borderTopColor: 'rgba(242, 234, 225, 1)',
+		borderTopWidth: 1,
+		backgroundColor: '#ffffff',
+		paddingLeft: 24,
+		paddingRight: 24,
+		flexDirection: 'row',
+		justifyContent: 'space-around'
+	},
+	cardActionButton: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		paddingTop: 16,
+		paddingBottom: 16
+	},
+	cardButtonIcon: {
+		paddingRight: 8
+	},
+	cardDivisor: {
+		width: '50%',
+		flexDirection: 'row',
+		alignItems: 'center',
+		borderRightColor: 'rgba(242, 234, 225, 1)',
+		borderRightWidth: 1
+	},
+	scrollviewHorizontal: {
+		paddingLeft: 24,
+		marginBottom: 32,
+		paddingBottom: 8
+	},
+	scrollviewHorizontalContent: {
+		flexDirection: 'row'
+	},
+	cardScroll: {
+		borderRadius: 16,
+		paddingLeft: 16,
+		paddingRight: 16,
+		marginRight: 16
+	}
+});
