7cf86f7ad055ccf99dd43a76b26f3be7f3f3a3ad
Marcos Teixeira <m.viniteixeira@gmail.com>
2022-01-14 11:41:52 +0000

    Fix actions panel

--- src/screens/home/WalletScreen.tsx
+++ src/screens/home/WalletScreen.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { SafeAreaView, ScrollView } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '@helpers/param-list-type';
 import Container from '@components/Container';
+import { useState } from '@hookstate/core';
+import { globalWalletState } from '@stores/WalletStore';
+import { walletDelete } from '@models/wallet';
 import styles from './styles';
 import Header from './header/Header';
 import AssetsPanel from './assets-panel/AssetsPanel';
@@ -10,13 +13,23 @@ import ActionsPanel from './actions-panel/ActionsPanel';
 import FinancePanel from './finance-panel/FinancePanel';
 
 export function WalletScreen({ navigation }: NativeStackScreenProps<RootStackParamList>) {
+	const state = useState(globalWalletState());
+	const onDeleteWallet = useCallback(async () => {
+		await walletDelete(state.value?.walletId || '');
+		state.set({ wallet: null, walletId: null });
+		navigation.navigate('Welcome');
+	}, [navigation]);
+
+	const onExchange = () => {
+		navigation.navigate('Exchange');
+	};
 	return (
 		<Container>
 			<Header />
 			<SafeAreaView>
 				<ScrollView style={styles.homeScroll}>
 					<AssetsPanel />
-					<ActionsPanel navigation={navigation} />
+					<ActionsPanel onDeleteWallet={onDeleteWallet} onExchange={onExchange} />
 					<FinancePanel />
 				</ScrollView>
 			</SafeAreaView>
--- src/screens/home/actions-panel/ActionsPanel.tsx
+++ src/screens/home/actions-panel/ActionsPanel.tsx
@@ -1,11 +1,6 @@
-import React, { useCallback } from 'react';
-import { SafeAreaView, ScrollView, View, StyleSheet } from 'react-native';
+import React from 'react';
+import { SafeAreaView, ScrollView, View, StyleSheet, GestureResponderEvent } from 'react-native';
 import RoundButton from '@components/RoundButton';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { RootStackParamList } from '@helpers/param-list-type';
-import { useState } from '@hookstate/core';
-import { globalWalletState } from '@stores/WalletStore';
-import { walletDelete } from '@models/wallet';
 
 const styles = StyleSheet.create({
 	scrollviewHorizontal: {
@@ -19,37 +14,30 @@ const styles = StyleSheet.create({
 	}
 });
 
-const ActionsPanel = ({ navigation }: NativeStackScreenProps<RootStackParamList>) => {
-	const state = useState(globalWalletState());
-	const onDeleteWallet = useCallback(async () => {
-		await walletDelete(state.value?.walletId || '');
-		state.set({ wallet: null, walletId: null });
-		navigation.navigate('Welcome');
-	}, [navigation]);
-
-	const onExchange = () => {
-		navigation.navigate('Exchange');
-	};
-
-	return (
-		<SafeAreaView>
-			<ScrollView
-				style={styles.scrollviewHorizontal}
-				horizontal
-				showsVerticalScrollIndicator={false}
-				showsHorizontalScrollIndicator={false}
-			>
-				<View style={styles.scrollviewHorizontalContent}>
-					<RoundButton text="Exchange" icon="compare-arrows" onPress={onExchange} />
-					<RoundButton text="Receive" icon="arrow-circle-down" />
-					<RoundButton text="Copy address" icon="content-copy" />
-					<RoundButton text="New wallet" icon="add" />
-					<RoundButton text="Switch accounts" icon="person-outline" />
-					<RoundButton text="Delete wallet" icon="delete-outline" onPress={onDeleteWallet} />
-				</View>
-			</ScrollView>
-		</SafeAreaView>
-	);
-};
+const ActionsPanel = ({
+	onDeleteWallet,
+	onExchange
+}: {
+	onDeleteWallet: (event: GestureResponderEvent) => void;
+	onExchange: (event: GestureResponderEvent) => void;
+}) => (
+	<SafeAreaView>
+		<ScrollView
+			style={styles.scrollviewHorizontal}
+			horizontal
+			showsVerticalScrollIndicator={false}
+			showsHorizontalScrollIndicator={false}
+		>
+			<View style={styles.scrollviewHorizontalContent}>
+				<RoundButton text="Exchange" icon="compare-arrows" onPress={onExchange} />
+				<RoundButton text="Receive" icon="arrow-circle-down" />
+				<RoundButton text="Copy address" icon="content-copy" />
+				<RoundButton text="New wallet" icon="add" />
+				<RoundButton text="Switch accounts" icon="person-outline" />
+				<RoundButton text="Delete wallet" icon="delete-outline" onPress={onDeleteWallet} />
+			</View>
+		</ScrollView>
+	</SafeAreaView>
+);
 
 export default ActionsPanel;
