9063f9531101cfdff32626ea61d39c7dd73ed65c
Marcos Teixeira <m.viniteixeira@gmail.com>
2022-01-24 11:59:26 +0000

    Fetching zapper info from polygon / ethereum

--- src/model/wallet.ts
+++ src/model/wallet.ts
@@ -193,9 +193,9 @@ export const estimateGas = async (): Promise<EstimateGasResponse> => {
 
 export const getWalletTokens = async (wallet: string): Promise<WalletTokensResponse> => {
 	const apiKey = '96e0cc51-a62e-42ca-acee-910ea7d2a241';
-	const result = await fetch(
-		`https://api.zapper.fi/v1/protocols/tokens/balances?api_key=${apiKey}&addresses[]=${wallet}`
-	);
+	const { zapperNetwork } = await selectedNetwork();
+	const baseURL = 'https://api.zapper.fi/v1/protocols/tokens/balances';
+	const result = await fetch(`${baseURL}?api_key=${apiKey}&addresses[]=${wallet}&network=${zapperNetwork}`);
 	return result.json();
 };
 
