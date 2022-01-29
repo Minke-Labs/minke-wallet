a64c71458ea47b0444879dcd4ea41bfba5aa66bb
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-01-29 21:13:48 +0900

    feat: fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import { ThemeProvider } from '@contexts';
 import Routes from '@routes';
+import AppLoading from 'expo-app-loading';
+import {
+	Inter_400Regular,
+	Inter_700Bold,
+	Inter_800ExtraBold,
+	Inter_500Medium,
+	useFonts
+} from '@expo-google-fonts/inter';
 
-const App = () => (
-	<ThemeProvider>
-		<Routes />
-	</ThemeProvider>
-);
+const App = () => {
+	const [fontsLoaded] = useFonts({
+		Inter_400Regular,
+		Inter_700Bold,
+		Inter_800ExtraBold,
+		Inter_500Medium
+	});
+	if (!fontsLoaded) return <AppLoading />;
+
+	return (
+		<ThemeProvider>
+			<Routes />
+		</ThemeProvider>
+	);
+};
 
 export default App;
