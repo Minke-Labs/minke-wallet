05272fc83bf232692478ea629f6886e8d5bde7b8
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-01-31 22:31:01 +0900

    update: Token component

--- src/screens/WelcomeScreen/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen/WelcomeScreen.tsx
@@ -32,7 +32,7 @@ const WelcomeScreen = () => {
 						<Text weight="extraBold"> Minke </Text>
 					</Text>
 					{/* <PrimaryButton onPress={() => console.log('CLICOU!!!')}>Create Wallet</PrimaryButton> */}
-					<Token name="xbc" size={200} />
+					<Token name="zilla" size={200} />
 				</View>
 			</View>
 		</WelcomeTemplate>
