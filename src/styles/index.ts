fbbbf3b0c64e99dbb8b954e4ea35b58575f8183c
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-02-06 18:21:51 +0900

    Completely removed react-native-paper's theme from project.

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,8 +1,6 @@
-import { lightTheme, darkTheme } from './theme';
 import { icon, IconType } from './icons';
 import { token, TokenType } from './tokens';
 import { colors, ColorType } from './colors';
 
-export { lightTheme, darkTheme, icon, token, colors };
-
+export { icon, token, colors };
 export type { IconType, TokenType, ColorType };
