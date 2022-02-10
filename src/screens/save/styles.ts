b2c588472b880a66cda53f3b36df41207559dee4
avelar <fortunato.avelar@gmail.com>
2022-02-09 23:21:01 -0300

    improve DepositAmount card styles; add makeStyles for darkmode

--- src/screens/save/styles.ts
+++ src/screens/save/styles.ts
@@ -1,73 +1,97 @@
 import { StyleSheet } from 'react-native';
 
-export default StyleSheet.create({
-	container: {
-		flex: 1,
-		flexDirection: 'column',
-		justifyContent: 'space-between',
-		backgroundColor: '#FFF'
-	},
-	background: {
-		flex: 1
-	},
-	saveCurrentValueContainer: {
-		flex: 1,
-		flexDirection: 'column',
-		padding: 24,
-		alignItems: 'center',
-		alignContent: 'center',
-		justifyContent: 'center'
-	},
-	row: {
-		flexDirection: 'row',
-		justifyContent: 'center',
-		alignItems: 'center'
-	},
-	depositButton: {
-		paddingTop: 16,
-		paddingRight: 32,
-		paddingBottom: 16,
-		paddingLeft: 32,
-		backgroundColor: '#fff'
-	},
-	saveCurrentValue: {
-		fontSize: 48,
-		marginTop: 8,
-		marginBottom: 24
-	},
-	depositCardContainer: {
-		backgroundColor: '#F2EAE1',
-		borderTopLeftRadius: 24,
-		borderTopRightRadius: 24,
-		paddingTop: 32,
-		paddingRight: 24,
-		paddingBottom: 32,
-		paddingLeft: 24
-	},
-	depositCard: {
-		borderRadius: 16,
-		backgroundColor: '#FFF',
-		padding: 24,
-		marginBottom: 16
-	},
-	actionDepositCard: {
-		borderRadius: 16,
-		backgroundColor: '#FFF',
-		padding: 24,
-		alignItems: 'center',
-		justifyContent: 'center'
-	},
-	actionDepositCardInfo: {
-		paddingTop: 40,
-		paddingBottom: 40
-	},
-	cardInfo: {
-		color: '#748190',
-		fontSize: 16,
-		marginBottom: 16
-	},
-	cardCta: {
-		color: '#4E5E6F',
-		fontSize: 16
-	}
-});
+export const makeStyles = (colors: ReactNativePaper.ThemeColors) =>
+	StyleSheet.create({
+		container: {
+			flex: 1,
+			flexDirection: 'column',
+			justifyContent: 'space-between',
+			backgroundColor: colors.fill
+		},
+		background: {
+			flex: 1
+		},
+		saveCurrentValueContainer: {
+			flex: 1,
+			flexDirection: 'column',
+			padding: 24,
+			alignItems: 'center',
+			alignContent: 'center',
+			justifyContent: 'center'
+		},
+		row: {
+			flexDirection: 'row',
+			justifyContent: 'center',
+			alignItems: 'center'
+		},
+		depositButton: {
+			paddingTop: 16,
+			paddingRight: 32,
+			paddingBottom: 16,
+			paddingLeft: 32,
+			backgroundColor: colors.fill
+		},
+		saveCurrentValue: {
+			fontSize: 48,
+			marginTop: 8,
+			marginBottom: 24
+		},
+		depositCardContainer: {
+			backgroundColor: colors.background,
+			// backgroundColor: '#FFF',
+			borderTopLeftRadius: 24,
+			borderTopRightRadius: 24,
+			paddingTop: 32,
+			paddingRight: 24,
+			paddingBottom: 32,
+			paddingLeft: 24
+		},
+		depositCard: {
+			borderRadius: 16,
+			backgroundColor: colors.fill,
+			marginBottom: 16,
+			flexDirection: 'row',
+			alignContent: 'center',
+			justifyContent: 'center'
+		},
+		depositCardItem: {
+			width: '50%',
+			padding: 24
+		},
+		divisor: {
+			borderRightWidth: 1,
+			flexGrow: 1,
+			flexBasis: 0,
+			justifyContent: 'center',
+			borderRightColor: colors.background
+		},
+		depositsAmount: {
+			color: colors.text,
+			fontSize: 24,
+			fontFamily: 'Inter_800ExtraBold',
+			marginTop: 8
+		},
+		depositsAmountLebel: {
+			color: colors.text
+		},
+		actionDepositCard: {
+			borderRadius: 16,
+			backgroundColor: colors.fill,
+			padding: 24,
+			alignItems: 'center',
+			justifyContent: 'center'
+		},
+		actionDepositCardInfo: {
+			paddingTop: 40,
+			paddingBottom: 40
+		},
+		cardInfo: {
+			color: colors.secondaryText,
+			fontSize: 16,
+			marginBottom: 16
+		},
+		cardCta: {
+			color: colors.secondaryText,
+			fontSize: 16
+		}
+	});
