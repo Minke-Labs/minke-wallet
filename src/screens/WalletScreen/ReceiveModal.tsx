6d07a5b9aa9733c749d50e02e4d71c3c53f74c91
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-02-12 01:01:16 +0900

    update: ReceiveModal.

--- src/screens/WalletScreen/ReceiveModal.tsx
+++ src/screens/WalletScreen/ReceiveModal.tsx
@@ -1,20 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, Share, SafeAreaView } from 'react-native';
+import { View, Share, SafeAreaView } from 'react-native';
 import { Text, WhiteButton } from '@components';
 import { getENSAddress } from '@models/wallet';
 import { globalWalletState } from '@stores/WalletStore';
 import QRCode from 'react-native-qrcode-svg';
-
-const styles = StyleSheet.create({
-	address: {
-		fontSize: 16,
-		marginBottom: 16
-	},
-	boldText: {
-		fontFamily: 'Inter_800ExtraBold',
-		fontSize: 24
-	}
-});
 
 const ReceiveModal = () => {
 	const wallet = globalWalletState();
@@ -39,32 +28,34 @@ const ReceiveModal = () => {
 				<Text type="h3" weight="extraBold" style={{ width: '100%' }}>
 					Receive
 				</Text>
-				<Text marginBottom={24} style={{ width: '100%' }}>
+				<Text marginBottom={44} style={{ width: '100%' }}>
 					Show your QR code or share your informations
 				</Text>
 				<View
 					style={{
 						borderRadius: 20,
 						backgroundColor: '#FFFFFF',
-						padding: 40,
-						marginTop: 30,
-						marginBottom: 30,
 						alignItems: 'center',
-						alignSelf: 'center',
-						width: '90%'
+						justifyContent: 'center',
+						width: 280,
+						height: 280,
+						marginBottom: 24
 					}}
 				>
-					<QRCode value={address} size={280} color="#34769D" />
+					<QRCode value={address} size={216} color="#34769D" />
 				</View>
 				<View
 					style={{
 						alignItems: 'center',
-						marginBottom: 30,
-						marginTop: 30
+						marginBottom: 30
 					}}
 				>
-					{ensName ? <Text style={styles.boldText}>{ensName}</Text> : null}
-					<Text style={styles.address}>{address}</Text>
+					{ensName ? (
+						<Text weight="extraBold" type="h3">
+							{ensName}
+						</Text>
+					) : null}
+					<Text>{address}</Text>
 				</View>
 				<WhiteButton title="Share" icon="shareStroke" onPress={onShare} />
 			</View>
