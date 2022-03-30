b8efbae790bd5a7cafc13c887316af8f0ab10834
Marcos Teixeira <m.viniteixeira@gmail.com>
2022-03-30 09:59:25 +0100

    POC

--- src/screens/Test/Test.tsx
+++ src/screens/Test/Test.tsx
@@ -16,56 +16,54 @@ const Test = () => {
 			address,
 			network: { chainId }
 		} = globalWalletState().value;
-		const lala = await getProvider();
-		const biconomy = new Biconomy(lala, {
+
+		const url = 'https://polygon-mainnet.g.alchemy.com/v2/oCWOrhTijsp7Ir1Qm9If2xxWgNa_YIcV';
+		const provider = new ethers.providers.JsonRpcProvider(url);
+		const biconomy = new Biconomy(provider, {
 			apiKey: 'fny6DM1HC.2dd130ac-5d62-4907-af68-17568cb95634',
 			debug: true
-		});
-		const provider = new ethers.providers.Web3Provider(biconomy);
-		const wallet = new Wallet(privateKey, provider);
-		const txBuilder = new TxBuilderV2(Network.polygon, provider);
-		const lendingPool = txBuilder.getLendingPool(Market.Proto);
-
-		const reserve = '0x8f3cf7ad23cd3cadbd9735aff958023239c6a063'; // DAI, not amDAI
-		const amount = '0.01';
-		console.log({ amount });
-		const result: EthereumTransactionTypeExtended[] = await lendingPool.deposit({
-			user: address,
-			reserve,
-			amount,
-			onBehalfOf: '0x5f5e3148532d1682866131a1971bb74a92d96376'
 		});
 
 		biconomy
 			.onEvent(biconomy.READY, async () => {
+				const ethersProvider = new ethers.providers.Web3Provider(biconomy);
+				const wallet = new Wallet(privateKey, ethersProvider);
+				const txBuilder = new TxBuilderV2(Network.polygon, provider);
+				const lendingPool = txBuilder.getLendingPool(Market.Proto);
+
+				const reserve = '0x8f3cf7ad23cd3cadbd9735aff958023239c6a063'; // DAI, not amDAI
+				const amount = '0.01';
 				// Initialize your dapp here like getting user accounts etc
+				const result: EthereumTransactionTypeExtended[] = await lendingPool.deposit({
+					user: address,
+					reserve,
+					amount,
+					onBehalfOf: '0x5f5e3148532d1682866131a1971bb74a92d96376'
+				});
 				const { tx } = result[0];
 				const nonce = await wallet.provider.getTransactionCount(wallet.address, 'latest');
-				let txDefaults;
+				let txDefaults = {};
 				try {
 					txDefaults = await tx();
 				} catch ({ transaction }) {
 					txDefaults = transaction;
 				}
 
-				console.log('pegou', txDefaults);
 				delete txDefaults.accessList;
-				const gasParams = {
-					gasPrice: parseUnits('60', 'gwei'),
-					gasLimit: 5000000
-				};
-				console.log({ txDefaults });
-				const transaction = { nonce, chainId, ...txDefaults, ...gasParams };
-				console.log({ transaction });
+				// const gasParams = {
+				// 	gasPrice: parseUnits('60', 'gwei'),
+				// 	gasLimit: 5000000
+				// };
+				const transaction = { nonce, chainId, ...txDefaults };
 				const signedTx = await wallet.signTransaction(transaction);
-				const { hash, wait } = await provider.sendTransaction(signedTx as string);
+				const hash = await ethersProvider.sendTransaction(signedTx as string);
 				if (hash) {
 					console.log(hash);
-					await wait();
 					console.log('finished');
 				}
 			})
 			.onEvent(biconomy.ERROR, (error: any, message: any) => {
+				console.log('Error');
 				console.error({ error });
 				console.log({ message });
 			});
