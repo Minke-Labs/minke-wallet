85cf60baaf1fa8b34b743f6b9ab188fce2661baf
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-04-06 16:30:42 +0900

    adjustments

--- src/components/CardTransaction/TransactionIcon/IconInner/IconInner.styles.ts
+++ src/components/CardTransaction/TransactionIcon/IconInner/IconInner.styles.ts
@@ -1,12 +1,21 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-	container: {
-		width: 12,
-		height: 12,
-		borderRadius: 6,
+	base: {
 		position: 'absolute',
 		bottom: 0,
-		right: 0
+		right: 0,
+		backgroundColor: 'white',
+		width: 14,
+		height: 14,
+		borderRadius: 7,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	pending: {
+		position: 'absolute',
+		backgroundColor: 'black',
+		width: 6,
+		height: 6
 	}
 });
--- src/components/CardTransaction/TransactionIcon/IconInner/IconInner.tsx
+++ src/components/CardTransaction/TransactionIcon/IconInner/IconInner.tsx
@@ -1,39 +1,17 @@
 import React from 'react';
 import { View } from 'react-native';
 import Icon from '../../../Icon/Icon';
+import { styles } from './IconInner.styles';
 import { IconInnerProps } from './IconInner.types';
 
-const Base: React.FC = ({ children }) => (
-	<View
-		style={{
-			position: 'absolute',
-			bottom: 0,
-			right: 0,
-			backgroundColor: 'white',
-			width: 14,
-			height: 14,
-			borderRadius: 7,
-			alignItems: 'center',
-			justifyContent: 'center'
-		}}
-	>
-		{children}
-	</View>
-);
+const Base: React.FC = ({ children }) => <View style={styles.base}>{children}</View>;
 
 // TODO: Add the righ icons when the design is ready.
 const IconInner: React.FC<IconInnerProps> = ({ pending, failed }) => {
 	if (pending && !failed) {
 		return (
 			<Base>
-				<View
-					style={{
-						position: 'absolute',
-						backgroundColor: 'black',
-						width: 6,
-						height: 6
-					}}
-				/>
+				<View style={styles.pending} />
 				<Icon size={12} name="pending" color="detail2" />
 			</Base>
 		);
