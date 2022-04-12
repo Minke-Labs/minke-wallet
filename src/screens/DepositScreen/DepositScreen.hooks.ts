6f7795384b22d33017fb10bd4105639efb9c9e7c
Marcos Teixeira <m.viniteixeira@gmail.com>
2022-04-12 16:53:41 +0100

    Temp fix for Zapper API not loading

--- src/screens/DepositScreen/DepositScreen.hooks.ts
+++ src/screens/DepositScreen/DepositScreen.hooks.ts
@@ -12,9 +12,8 @@ export const useDepositScreen = () => {
 	const [approved, setApproved] = React.useState<boolean | undefined>(); // transaction amount is approved?
 	const [notAbleToSaveVisible, setNotAbleToSaveVisible] = React.useState(true);
 	const [addFundsVisible, setAddFundsVisible] = React.useState(false);
-	const {
-		market: { tokens = [], address: marketAddress }
-	} = useState(globalDepositState()).value;
+	const { market } = useState(globalDepositState()).value;
+	const { tokens = [], address: marketAddress } = market || {};
 	const { address } = globalWalletState().value;
 
 	const { ableToDeposit } = useDeposit();
