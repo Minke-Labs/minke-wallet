3fdb02130ddb7f26e191f6222174b4286cd702c1
Marcos Teixeira <m.viniteixeira@gmail.com>
2022-04-22 13:20:36 +0200

    Checking if the wallet has enough gasless limit

--- src/screens/Test/Test.tsx
+++ src/screens/Test/Test.tsx
@@ -1,41 +1,26 @@
 import React from 'react';
 import { View, Button } from 'react-native';
-import { Text } from '@components';
 import { BasicLayout } from '@layouts';
-import * as Haptics from 'expo-haptics';
+import { useBiconomy } from '@hooks';
+import { globalWalletState } from '@stores/WalletStore';
 
-const Test = () => (
-	<BasicLayout>
-		<View style={{ paddingTop: 160, paddingHorizontal: 24 }}>
-			<View>
-				<Text>Haptics.selectionAsync</Text>
-				<View>
-					<Button title="Selection" onPress={() => Haptics.selectionAsync()} />
-				</View>
-				<Text>Haptics.notificationAsync</Text>
-				<View>
-					<Button
-						title="Success"
-						onPress={() => Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success)}
-					/>
-					<Button
-						title="Error"
-						onPress={() => Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error)}
-					/>
-					<Button
-						title="Warning"
-						onPress={() => Haptics.notificationAsync(Haptics.NotificationFeedbackType.Warning)}
-					/>
-				</View>
-				<Text>Haptics.impactAsync</Text>
-				<View>
-					<Button title="Light" onPress={() => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)} />
-					<Button title="Medium" onPress={() => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)} />
-					<Button title="Heavy" onPress={() => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy)} />
-				</View>
+const Test = () => {
+	const { gaslessEnabled, canSubmitGaslessTransaction } = useBiconomy();
+	const { address } = globalWalletState();
+	const test = async () => {
+		const allowed = await canSubmitGaslessTransaction(address);
+		console.log({ allowed });
+	};
+
+	console.log({ gaslessEnabled });
+
+	return (
+		<BasicLayout>
+			<View style={{ paddingTop: 160, paddingHorizontal: 24 }}>
+				<Button title="Test" onPress={test} />
 			</View>
-		</View>
-	</BasicLayout>
-);
+		</BasicLayout>
+	);
+};
 
 export default Test;
