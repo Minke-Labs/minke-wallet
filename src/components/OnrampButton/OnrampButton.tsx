0ec23a759c25a29ec41649dbaee6b9ec6b3da8ad
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-05-04 10:12:42 +0900

    Pay with

--- src/components/OnrampButton/OnrampButton.tsx
+++ src/components/OnrampButton/OnrampButton.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Image, Text, TouchableOpacity } from 'react-native';
-import { useLocation } from '@hooks';
+import { useLocation, useLanguage } from '@hooks';
 import styles from './OnrampButton.styles';
 import { chooseLocation } from './OnrampButton.utils';
 import { OnrampButtonProps } from './OnrampButton.types';
 
 const OnrampButton: React.FC<OnrampButtonProps> = ({ onPress, marginBottom = 0, disabled = false }) => {
 	const { countryCode } = useLocation();
+	const { i18n } = useLanguage();
 
 	if (!chooseLocation(countryCode!)) return null;
 
@@ -28,7 +29,7 @@ const OnrampButton: React.FC<OnrampButtonProps> = ({ onPress, marginBottom = 0, 
 					{ color: chooseLocation(countryCode!)?.fontColor }
 				]}
 			>
-				Pay With
+				{i18n.t('Components.Buttons.pay_with')}
 			</Text>
 			<Image source={chooseLocation(countryCode!)?.image} style={styles.image} />
 		</TouchableOpacity>
