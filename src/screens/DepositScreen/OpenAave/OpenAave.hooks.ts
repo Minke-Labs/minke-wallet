c9e2051b14bb0f1ff99104fab319be5901138dd8
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-05-12 11:30:54 +0900

    OpenAave

--- src/screens/DepositScreen/OpenAave/OpenAave.hooks.ts
+++ src/screens/DepositScreen/OpenAave/OpenAave.hooks.ts
@@ -9,6 +9,7 @@ import Logger from '@utils/logger';
 import { aaveDepositContract, gaslessApproval } from '@models/gaslessTransaction';
 
 export const useOpenAave = ({ onApprove }: { onApprove: () => void }) => {
+	const [blockchainError, setBlockchainError] = useState(false);
 	const { biconomy, gaslessEnabled } = useBiconomy();
 	const [loading, setLoading] = useState(false);
 	const { address, privateKey } = globalWalletState().value;
@@ -21,20 +22,25 @@ export const useOpenAave = ({ onApprove }: { onApprove: () => void }) => {
 		setLoading(true);
 
 		if (gaslessEnabled) {
-			const hash = await gaslessApproval({
-				address,
-				privateKey,
-				biconomy,
-				contract: tokens[0].address,
-				spender: aaveDepositContract
-			});
-			if (hash) {
-				track('Opened AAVE Account', { hash, gasless: true });
+			try {
+				const hash = await gaslessApproval({
+					address,
+					privateKey,
+					biconomy,
+					contract: tokens[0].address,
+					spender: aaveDepositContract
+				});
+				if (hash) {
+					track('Opened AAVE Account', { hash, gasless: true });
+					setLoading(false);
+					onApprove();
+				} else {
+					Logger.error('Error approving');
+					setLoading(false);
+				}
+			} catch (e) {
 				setLoading(false);
-				onApprove();
-			} else {
-				Logger.error('Error approving');
-				setLoading(false);
+				setBlockchainError(true);
 			}
 		} else {
 			const transaction = await approvalTransaction(address, tokens[0].address);
@@ -56,18 +62,22 @@ export const useOpenAave = ({ onApprove }: { onApprove: () => void }) => {
 				chainId
 			};
 
-			Logger.log(`Approval ${JSON.stringify(txDefaults)}`);
+			try {
+				Logger.log(`Approval ${JSON.stringify(txDefaults)}`);
+				const signedTx = await wallet.signTransaction(txDefaults);
+				const { hash, wait } = await provider.sendTransaction(signedTx as string);
+				if (hash) {
+					track('Opened AAVE Account', { hash, gasless: false });
 
-			const signedTx = await wallet.signTransaction(txDefaults);
-			const { hash, wait } = await provider.sendTransaction(signedTx as string);
-			if (hash) {
-				track('Opened AAVE Account', { hash, gasless: false });
-
-				await wait();
-				setLoading(false);
-				onApprove();
-			} else {
-				Logger.error('Error approving');
+					await wait();
+					setLoading(false);
+					onApprove();
+				} else {
+					Logger.error('Error approving');
+					setLoading(false);
+				}
+			} catch (e) {
+				setBlockchainError(true);
 				setLoading(false);
 			}
 		}
@@ -76,6 +86,8 @@ export const useOpenAave = ({ onApprove }: { onApprove: () => void }) => {
 	return {
 		loading,
 		onOpenAccount,
-		gaslessEnabled
+		gaslessEnabled,
+		blockchainError,
+		setBlockchainError
 	};
 };
--- src/screens/DepositScreen/OpenAave/OpenAave.tsx
+++ src/screens/DepositScreen/OpenAave/OpenAave.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { Linking, TouchableOpacity, View, SafeAreaView } from 'react-native';
-import { Icon, Text, HapticButton, TransparentCard, ActivityIndicator } from '@components';
+import { Icon, Text, HapticButton, TransparentCard, ActivityIndicator, ModalReusables, Modal } from '@components';
 import { useAmplitude, useNavigation, useLanguage } from '@hooks';
 import { BasicLayout } from '@layouts';
 import styles from './OpenAave.styles';
@@ -11,79 +11,99 @@ const OpenAave = ({ onApprove }: { onApprove: () => void }) => {
 	const { i18n } = useLanguage();
 	const { track } = useAmplitude();
 	const navigation = useNavigation();
-	const { loading, onOpenAccount, gaslessEnabled } = useOpenAave({ onApprove });
+	const {
+		loading,
+		onOpenAccount,
+		gaslessEnabled,
+		blockchainError,
+		setBlockchainError
+	} = useOpenAave({ onApprove });
 
 	useEffect(() => {
 		track('OpenAave Screen Opened');
 	}, []);
 
 	return (
-		<BasicLayout hideSafeAreaView>
-			<Background>
-				<View style={styles.container}>
-					<SafeAreaView>
-						<View style={styles.headerNavigation}>
-							<TouchableOpacity onPress={() => navigation.goBack()}>
-								<Icon name="arrowBackStroke" color="text7" size={24} />
-							</TouchableOpacity>
+		<>
+			<BasicLayout hideSafeAreaView>
+				<Background>
+					<View style={styles.container}>
+						<SafeAreaView>
+							<View style={styles.headerNavigation}>
+								<TouchableOpacity onPress={() => navigation.goBack()}>
+									<Icon name="arrowBackStroke" color="text7" size={24} />
+								</TouchableOpacity>
+							</View>
+
+							<Text type="h3" weight="bold" color="text1" marginBottom={35}>
+								{i18n.t('DepositScreen.OpenAave.open_aave')}
+							</Text>
+
+							<TransparentCard marginBottom={8}>
+								<Text weight="extraBold" marginBottom={12} style={{ width: '100%' }}>
+									{i18n.t('DepositScreen.OpenAave.what_is')}
+								</Text>
+								<Text type="a">
+									{i18n.t('DepositScreen.OpenAave.aave_des')}
+								</Text>
+							</TransparentCard>
+
+							<View style={styles.actionContainer}>
+								<TouchableOpacity onPress={() => Linking.openURL('https://aave.com/')}>
+									<TransparentCard row padding={16}>
+										<Icon name="siteStroke" color="cta1" size={24} style={{ marginRight: 8 }} />
+										<Text type="a" color="text2">
+											{i18n.t('DepositScreen.OpenAave.view_site')}
+										</Text>
+										<Icon name="chevronRight" color="cta1" size={24} />
+									</TransparentCard>
+								</TouchableOpacity>
+
+								<TouchableOpacity onPress={() => Linking.openURL('https://docs.aave.com/faq/')}>
+									<TransparentCard row padding={16}>
+										<Icon name="learnStroke" color="cta1" size={24} style={{ marginRight: 8 }} />
+										<Text type="a" color="text2">
+											{i18n.t('DepositScreen.OpenAave.learn_more')}
+										</Text>
+										<Icon name="chevronRight" color="cta1" size={24} />
+									</TransparentCard>
+								</TouchableOpacity>
+							</View>
+						</SafeAreaView>
+						<View style={{ bottom: 34 }}>
+							<HapticButton
+								title={i18n.t('DepositScreen.OpenAave.open_account')}
+								marginBottom={16}
+								onPress={onOpenAccount}
+								disabled={loading}
+							/>
+							{!gaslessEnabled && (
+								<Text type="span" color="text2" style={{ textAlign: 'center' }}>
+									{i18n.t('DepositScreen.OpenAave.this_transaction')}
+								</Text>
+							)}
 						</View>
-
-						<Text type="h3" weight="bold" color="text1" marginBottom={35}>
-							{i18n.t('DepositScreen.OpenAave.open_aave')}
-						</Text>
-
-						<TransparentCard marginBottom={8}>
-							<Text weight="extraBold" marginBottom={12} style={{ width: '100%' }}>
-								{i18n.t('DepositScreen.OpenAave.what_is')}
-							</Text>
-							<Text type="a">
-								{i18n.t('DepositScreen.OpenAave.aave_des')}
-							</Text>
-						</TransparentCard>
-
-						<View style={styles.actionContainer}>
-							<TouchableOpacity onPress={() => Linking.openURL('https://aave.com/')}>
-								<TransparentCard row padding={16}>
-									<Icon name="siteStroke" color="cta1" size={24} style={{ marginRight: 8 }} />
-									<Text type="a" color="text2">
-										{i18n.t('DepositScreen.OpenAave.view_site')}
-									</Text>
-									<Icon name="chevronRight" color="cta1" size={24} />
-								</TransparentCard>
-							</TouchableOpacity>
-
-							<TouchableOpacity onPress={() => Linking.openURL('https://docs.aave.com/faq/')}>
-								<TransparentCard row padding={16}>
-									<Icon name="learnStroke" color="cta1" size={24} style={{ marginRight: 8 }} />
-									<Text type="a" color="text2">
-										{i18n.t('DepositScreen.OpenAave.learn_more')}
-									</Text>
-									<Icon name="chevronRight" color="cta1" size={24} />
-								</TransparentCard>
-							</TouchableOpacity>
-						</View>
-					</SafeAreaView>
-					<View style={{ bottom: 34 }}>
-						<HapticButton
-							title={i18n.t('DepositScreen.OpenAave.open_account')}
-							marginBottom={16}
-							onPress={onOpenAccount}
-							disabled={loading}
-						/>
-						{!gaslessEnabled && (
-							<Text type="span" color="text2" style={{ textAlign: 'center' }}>
-								{i18n.t('DepositScreen.OpenAave.this_transaction')}
-							</Text>
+						{loading && (
+							<View style={styles.loadingBox}>
+								<ActivityIndicator />
+							</View>
 						)}
 					</View>
-					{loading && (
-						<View style={styles.loadingBox}>
-							<ActivityIndicator />
-						</View>
-					)}
-				</View>
-			</Background>
-		</BasicLayout>
+				</Background>
+			</BasicLayout>
+
+			<Modal isVisible={blockchainError} onDismiss={() => setBlockchainError(false)}>
+				{blockchainError && (
+					<ModalReusables.Error
+						title={i18n.t('Components.ModalReusables.Error.Blockchain.title')}
+						description={i18n.t('Components.ModalReusables.Error.Blockchain.description')}
+						onDismiss={() => setBlockchainError(false)}
+						showHeader
+						buttonLabel={i18n.t('Components.ModalReusables.Error.Blockchain.button')}
+					/>
+				)}
+			</Modal>
+		</>
 	);
 };
 
