b906bf48f0e78d252386d7b6052407c91cdae396
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-05-19 15:35:32 +0900

    Onramp button now using SVGs

--- src/components/OnrampButton/OnrampButton.tsx
+++ src/components/OnrampButton/OnrampButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, Text, TouchableOpacity } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 import { useLanguage } from '@hooks';
 import styles from './OnrampButton.styles';
 import { chooseLocation } from './OnrampButton.utils';
@@ -9,22 +9,19 @@ const OnrampButton: React.FC<OnrampButtonProps> = ({ onPress, marginBottom = 0, 
 	const { i18n, countryCode } = useLanguage();
 	const location = chooseLocation(countryCode!);
 	if (!location) return null;
-
-	const { backgroundColor, fontColor, image } = location;
+	const { locStyles, image } = location;
+	const { backgroundColor, fontColor } = locStyles;
 	return (
 		<TouchableOpacity
 			activeOpacity={0.6}
 			style={[
 				styles.container,
-				{
-					backgroundColor,
-					marginBottom
-				}
+				{ backgroundColor, marginBottom }
 			]}
 			{...{ onPress, disabled }}
 		>
 			<Text style={[styles.text, { color: fontColor }]}>{i18n.t('Components.Buttons.pay_with')}</Text>
-			<Image source={image} style={styles.image} />
+			{image()}
 		</TouchableOpacity>
 	);
 };
