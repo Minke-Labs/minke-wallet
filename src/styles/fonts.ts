bb2ab4d22cb7df5bf6f5d1c984755d8ee8afbb1a
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-06-03 11:26:52 +0900

    Refactoring fonts

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,3 +1,4 @@
+export type FontWeightType = keyof typeof fontWeight;
 const fontWeight = {
 	regular: 'Inter_400Regular',
 	medium: 'Inter_500Medium',
@@ -6,6 +7,7 @@ const fontWeight = {
 	extraBold: 'Inter_800ExtraBold'
 };
 
+export type FontTypeType = keyof typeof fontType;
 const fontType = {
 	textLarge: {
 		fontSize: 48,
@@ -42,11 +44,31 @@ const fontType = {
 	},
 
 	// NEW SIZES
+
+	// Headline
+	hLarge: {
+		fontSize: 32,
+		lineHeight: 44.8
+	},
+	hMedium: {
+		fontSize: 24,
+		lineHeight: 33.6
+	},
 	hSmall: {
 		fontSize: 20,
 		lineHeight: 28
 	},
+	// Title
+	tMedium: {
+		fontSize: 18,
+		lineHeight: 21.78
+	},
 	tSmall: {
+		fontSize: 16,
+		lineHeight: 22.4
+	},
+	// Label
+	lLarge: {
 		fontSize: 16,
 		lineHeight: 22.4
 	},
@@ -57,6 +79,15 @@ const fontType = {
 	lSmall: {
 		fontSize: 12,
 		lineHeight: 16.8
+	},
+	// Body
+	bMedium: {
+		fontSize: 16,
+		lineHeight: 22.4
+	},
+	bSmall: {
+		fontSize: 14,
+		lineHeight: 19.6
 	}
 };
 
