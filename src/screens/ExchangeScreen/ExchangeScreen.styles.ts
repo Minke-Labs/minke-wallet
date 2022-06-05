218ced1ae6b23cd0622c14eab3b281555cc8af9e
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-06-05 22:20:42 +0900

    fix

--- src/screens/ExchangeScreen/ExchangeScreen.styles.ts
+++ src/screens/ExchangeScreen/ExchangeScreen.styles.ts
@@ -29,33 +29,5 @@ export const makeStyles = (colors: ColorType) =>
 			marginHorizontal: 16,
 			marginTop: 'auto',
 			marginBottom: 16
-		},
-
-		header: {
-			height: 48,
-			paddingHorizontal: 32,
-			flexDirection: 'row',
-			justifyContent: 'space-between',
-			alignItems: 'center'
-		},
-		textBold: {
-			fontFamily: 'Inter_800ExtraBold'
-		},
-		exchangeSection: {
-			paddingHorizontal: 24
-		},
-		exchangeButton: {
-			marginTop: 'auto',
-			marginBottom: 16
-		},
-		exchangeHeadlineRow: {
-			flexDirection: 'row',
-			justifyContent: 'space-between',
-			alignItems: 'center'
-		},
-		exchangeSummaryText: {
-			marginTop: 8,
-			fontSize: 12,
-			color: colors.cta2
 		}
 	});
