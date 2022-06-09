03629d5e90f020e49983fd03d86ae737a8218feb
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-06-09 11:38:29 +0900

    Moving useNetwork to context api

--- src/contexts/index.ts
+++ src/contexts/index.ts
@@ -2,6 +2,7 @@ import ThemeProvider, { ThemeContext } from './themeContext/ThemeContext';
 import AmplitudeProvider, { AmplitudeContext } from './AmplitudeContext/AmplitudeContext';
 import BiconomyProvider, { BiconomyContext } from './BiconomyContext/BiconomyContext';
 import LanguageProvider, { LanguageContext } from './LanguageContext/LanguageContext';
+import NetworkProvider, { NetworkContext } from './NetworkContext/NetworkContext';
 import TransactionsProvider, { TransactionsContext } from './TransactionsContext/TransactionsContext';
 
 export {
@@ -10,6 +11,7 @@ export {
 	LanguageContext,
 	BiconomyContext,
 	TransactionsContext,
+	NetworkContext,
 	TransactionsProvider
 };
 
@@ -17,5 +19,6 @@ export const providers = [
 	ThemeProvider,
 	AmplitudeProvider,
 	BiconomyProvider,
+	NetworkProvider,
 	LanguageProvider
 ];
