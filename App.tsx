d76c3ecd0fbc527abe635c64ee1331942a9493bc
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-06-11 19:13:55 +0900

    uxCam init

--- App.tsx
+++ App.tsx
@@ -4,8 +4,15 @@ import Routes from '@routes';
 import AppLoading from 'expo-app-loading';
 import Logger from '@utils/logger';
 import * as Sentry from '@sentry/react-native';
+import RNUxcam from 'react-native-ux-cam';
+import { UXCAM_API_KEY } from '@env';
 import { useApp } from './App.hooks';
 import { Providers } from './Providers';
+
+const uxCamKey = UXCAM_API_KEY || process.env.UXCAM_API_KEY;
+
+RNUxcam.optIntoSchematicRecordings();
+RNUxcam.startWithKey(uxCamKey!);
 
 Logger.initialize();
 
--- env.d.ts
+++ env.d.ts
@@ -22,4 +22,5 @@ declare module '@env' {
 	export const BANXA_KEY: string;
 	export const BANXA_SECRET: string;
 	export const APPS_FLYER_DEV_KEY: string;
+	export const UXCAM_API_KEY: string;
 }
