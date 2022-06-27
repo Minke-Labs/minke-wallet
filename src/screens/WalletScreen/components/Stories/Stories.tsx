da4ac0ab02cfc21971f68ad1a0cd49d119c4dc12
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-06-27 22:59:49 +0900

    Updates tiles

--- src/screens/WalletScreen/components/Stories/Stories.tsx
+++ src/screens/WalletScreen/components/Stories/Stories.tsx
@@ -1,10 +1,203 @@
-/* eslint-disable no-console */
 import React, { useState, useEffect, useRef } from 'react';
 import { View, TouchableOpacity, useColorScheme } from 'react-native';
 import { Text, Icon } from '@components';
 import { useLanguage, useWalletState } from '@hooks';
 import { STORYTELLER_KEY } from '@env';
-import Storyteller, { StorytellerRowView, UIStyle } from '@getstoryteller/react-native-storyteller-sdk';
+import Storyteller,
+{
+	StorytellerRowView,
+	UIStyle,
+	Theme,
+	ElementAlignment,
+	TextCase
+} from '@getstoryteller/react-native-storyteller-sdk';
+
+const storytellerTheme: Theme = {
+	light: {
+		colors: {
+			primary: '#F2EAE1',
+			success: '#30C061',
+			alert: '#C03030',
+			white: {
+				primary: '#FFFFFF'
+			},
+			black: {
+				primary: '#000000'
+			}
+		},
+		customFont: {
+			ios: {
+				regular: 'Comic Sans MS',
+				medium: 'Comic Sans MS',
+				semibold: 'Comic Sans MS Bold',
+				bold: 'Comic Sans MS Bold',
+				heavy: 'Comic Sans MS Bold',
+				black: 'Comic Sans MS Bold'
+			},
+			android: {
+				font: 'Comic Sans MS'
+			}
+		},
+		primitives: {
+			cornerRadius: 10
+		},
+		lists: {
+			row: {
+				tileSpacing: 12,
+				startInset: 0,
+				endInset: 0
+			},
+			grid: {
+				tileSpacing: 12,
+				columns: 0,
+				topInset: 0,
+				bottomInset: 0
+			},
+			home: {
+				startInset: 0,
+				endInset: 0
+			}
+		},
+		storyTiles: {
+			title: {
+				textSize: 12,
+				lineHeight: 16.8,
+				alignment: ElementAlignment.center
+			},
+			circularTile: {
+				readIndicatorColor: '#000000',
+				title: {}
+			},
+			rectangularTile: {
+				padding: 4,
+				title: {
+					textColor: '#000000'
+				},
+				unreadIndicator: {
+					alignment: ElementAlignment.right,
+					textSize: 12,
+					backgroundColor: '#30C061'
+				}
+			}
+		},
+		player: {
+			icons: {
+				share: false,
+				refresh: false
+			},
+			showStoryIcon: false,
+			showTimestamp: false,
+			showShareButton: false
+		},
+		buttons: {
+			textCase: TextCase.default
+		},
+		instructions: {
+			show: false,
+			button: {}
+		},
+		engagementUnits: {
+			poll: {
+				percentBarColor: '#FABA18',
+				showVoteCount: false,
+				showPercentBarBackground: false
+			},
+			triviaQuiz: {}
+		}
+	},
+	dark: {
+		colors: {
+			primary: '#0A2138',
+			success: '#ACE8B9',
+			alert: '#E18A8A',
+			white: {
+				primary: '#FFFFFF'
+			},
+			black: {
+				primary: '#000000'
+			}
+		},
+		customFont: {
+			ios: {
+				regular: 'Comic Sans MS',
+				medium: 'Comic Sans MS',
+				semibold: 'Comic Sans MS Bold',
+				bold: 'Comic Sans MS Bold',
+				heavy: 'Comic Sans MS Bold',
+				black: 'Comic Sans MS Bold'
+			},
+			android: {
+				font: 'Comic Sans MS'
+			}
+		},
+		primitives: {
+			cornerRadius: 10
+		},
+		lists: {
+			row: {
+				tileSpacing: 12,
+				startInset: 0,
+				endInset: 0
+			},
+			grid: {
+				tileSpacing: 12,
+				columns: 0,
+				topInset: 0,
+				bottomInset: 0
+			},
+			home: {
+				startInset: 0,
+				endInset: 0
+			}
+		},
+		storyTiles: {
+			title: {
+				textSize: 12,
+				lineHeight: 16.8,
+				alignment: ElementAlignment.center
+			},
+			circularTile: {
+				readIndicatorColor: '#FFFFFF',
+				title: {}
+			},
+			rectangularTile: {
+				padding: 4,
+				title: {
+					textColor: '#FFFFFF'
+				},
+				unreadIndicator: {
+					alignment: ElementAlignment.right,
+					textSize: 12,
+					backgroundColor: '#30C061'
+				}
+			}
+		},
+		player: {
+			icons: {
+				share: false,
+				refresh: false
+			},
+			showStoryIcon: false,
+			showTimestamp: false,
+			showShareButton: false
+		},
+		buttons: {
+			textCase: TextCase.default
+		},
+		instructions: {
+			show: false,
+			button: {}
+		},
+		engagementUnits: {
+			poll: {
+				percentBarColor: '#FABA18',
+				showVoteCount: false,
+				showPercentBarBackground: false
+			},
+			triviaQuiz: {}
+		}
+	}
+};
 
 const Stories: React.FC = () => {
 	const { state } = useWalletState();
@@ -56,6 +249,8 @@ const Stories: React.FC = () => {
 			</TouchableOpacity>
 			<View style={{ width: '100%', marginTop: 12, display: toggle ? 'flex' : 'none' }}>
 				<StorytellerRowView
+					cellType="round"
+					theme={storytellerTheme}
 					ref={rowRef}
 					style={{ height: 91 }}
 					uiStyle={scheme === 'dark' ? 'dark' as UIStyle : 'light' as UIStyle}
