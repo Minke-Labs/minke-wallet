c92e2af1af797225e2ccac84cf28dde70acad584
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-07-07 20:33:53 +0900

    fix - only images

--- src/contexts/AvatarContext/AvatarContext.tsx
+++ src/contexts/AvatarContext/AvatarContext.tsx
@@ -89,9 +89,10 @@ const AvatarProvider: React.FC = ({ children }) => {
 
 	const pickImage = async () => {
 		const result = await ImagePicker.launchImageLibraryAsync({
-			mediaTypes: ImagePicker.MediaTypeOptions.All,
+			mediaTypes: ImagePicker.MediaTypeOptions.Images,
 			allowsEditing: true,
 			aspect: [4, 3],
+			exif: false,
 			quality: 0
 		});
 
