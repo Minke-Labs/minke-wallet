b41a21f8fa3b7720e0b79178d7547965e7ea3c5e
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-07-19 20:40:58 +0900

    fix - spacing

--- src/containers/AddFunds/CountryModal/CountryModal.tsx
+++ src/containers/AddFunds/CountryModal/CountryModal.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
+import KeyboardSpacer from 'react-native-keyboard-spacer';
 import { CountrySelector } from '@components';
 
 const CountryModal = () => (
 	<View>
 		<CountrySelector limitHeight desc />
+		<KeyboardSpacer />
 	</View>
 );
 
