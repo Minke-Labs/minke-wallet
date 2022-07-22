7285546bae569a5ed79a64238276947a0171a6d6
Marcos Teixeira <m.viniteixeira@gmail.com>
2022-07-22 14:06:11 +0200

    Widget POC

--- src/screens/Test/Test.tsx
+++ src/screens/Test/Test.tsx
@@ -1,32 +1,34 @@
 import React from 'react';
 import * as qs from 'qs';
 import WebView from 'react-native-webview';
-import { useColorScheme, View } from 'react-native';
-import { ON_RAMPER_API_KEY } from '@env';
+import { View } from 'react-native';
+import { MOONPAY_API_URL, MOONPAY_API_KEY, MOONPAY_SECRET_KEY } from '@env';
 import { Button, FullModal } from '@components';
 import { BasicLayout } from '@layouts';
+import { useState } from '@hookstate/core';
+import crypto from 'crypto';
+import { globalWalletState } from '@stores/WalletStore';
 
 const Test = () => {
 	const [visible, setVisible] = React.useState(false);
-	const apiKey = ON_RAMPER_API_KEY || process.env.ON_RAMPER_API_KEY;
-	const scheme = useColorScheme();
+	const apiKey = MOONPAY_API_KEY || process.env.MOONPAY_API_KEY;
+	const { address } = useState(globalWalletState()).value;
 
 	const params = {
 		apiKey,
-		supportSell: false,
-		defaultCrypto: 'ETH',
-		defaultFiat: 'USD',
-		defaultAmount: 100,
-		wallets: 'ETH:0xad0637645341a160c4621a5ae22a709feca37234', // wallet in downcase
-		onlyCryptos: 'ETH, MATIC, USDC, USDC_POLYGON', // split by network
-		isAddressEditable: false,
-		isAmountEditable: false,
-		color: '0A2138',
-		fontFamily: 'Inter',
-		darkMode: scheme === 'dark'
+		currencyCode: 'ETH',
+		walletAddress: address,
+		baseCurrencyCode: 'usd',
+		baseCurrencyAmount: 159,
+		lockAmount: true
 	};
-	const url = `https://widget.onramper.com?${qs.stringify(params)}`;
-	console.log(url);
+
+	const query = `?${qs.stringify(params)}`;
+	const host = MOONPAY_API_URL || process.env.MOONPAY_API_URL;
+	const originalUrl = `${host}${query}`;
+	const secret = MOONPAY_SECRET_KEY || process.env.MOONPAY_SECRET_KEY;
+	const signature = crypto.createHmac('sha256', secret!).update(query).digest('base64');
+	const urlWithSignature = `${originalUrl}&signature=${encodeURIComponent(signature)}`;
 
 	return (
 		<>
@@ -36,7 +38,9 @@ const Test = () => {
 				</View>
 			</BasicLayout>
 			<FullModal visible={visible} onClose={() => setVisible(false)}>
-				{visible && <WebView enableApplePay useWebKit source={{ uri: url }} sharedCookiesEnabled />}
+				{visible && (
+					<WebView enableApplePay useWebKit source={{ uri: urlWithSignature }} sharedCookiesEnabled />
+				)}
 			</FullModal>
 		</>
 	);
