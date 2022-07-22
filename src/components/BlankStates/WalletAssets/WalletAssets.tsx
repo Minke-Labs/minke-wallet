8132c30e64c0ba6e06bb51c11d8bb4f3ba45a7aa
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-07-22 18:14:05 +0900

    fix - margin bottom

--- src/components/BlankStates/WalletAssets/WalletAssets.tsx
+++ src/components/BlankStates/WalletAssets/WalletAssets.tsx
@@ -5,7 +5,7 @@ import BlankLayout from '../BlankLayout/BlankLayout';
 const WalletAssets = () => (
 	<BlankLayout>
 		<Box mt={136} mb={16} w={105} h={14} br={20} />
-		<Box mb={16} w={191} h={42} br={60} />
+		<Box mb={64} w={191} h={42} br={60} />
 		<Box w="100%" style={{ flex: 1, borderTopLeftRadius: 22, borderTopRightRadius: 22 }} />
 	</BlankLayout>
 );
