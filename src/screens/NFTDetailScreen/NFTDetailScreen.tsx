f3d5084f517d9ad97b2c7e3a40870ce773db812a
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-07-26 15:14:42 +0900

    NFTDetailScreen init

---
+++ src/screens/NFTDetailScreen/NFTDetailScreen.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const NFTDetailScreen = () => (
+	<View>
+		<Text>NFTDetailScreen</Text>
+	</View>
+);
+
+export default NFTDetailScreen;
