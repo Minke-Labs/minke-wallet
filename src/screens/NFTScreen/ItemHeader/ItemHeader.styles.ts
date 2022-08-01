e358d85ebb441bf5fa638060364455d716ec0a70
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-08-01 16:13:07 +0900

    Improving on the data manipulation.

---
+++ src/screens/NFTScreen/ItemHeader/ItemHeader.styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+	container: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		marginBottom: 16
+	},
+	image: {
+		width: 32,
+		height: 32,
+		borderRadius: 16,
+		marginRight: 8
+	}
+});
