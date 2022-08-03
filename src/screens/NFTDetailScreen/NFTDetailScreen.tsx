6898f6faeb4d648031327fbee6d6e49beafb967d
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-08-03 18:07:41 +0900

    Last Sale problems fixed

--- src/screens/NFTDetailScreen/NFTDetailScreen.tsx
+++ src/screens/NFTDetailScreen/NFTDetailScreen.tsx
@@ -5,6 +5,7 @@ import { useLanguage, useTheme } from '@hooks';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '@src/routes/types.routes';
 import { getCollectionStats } from '@models/openSea';
+import { truncate } from '@src/hooks/useTransaction';
 import { Bottom } from './Bottom/Bottom';
 import { Expander } from './Expander/Expander';
 import { Panel } from './Panel/Panel';
@@ -20,29 +21,29 @@ const NFTDetailScreen = ({ route }: Props) => {
 
 	useEffect(() => {
 		const fetchAssets = async () => {
-			const NFTstats = await getCollectionStats('doodles-official');
+			const NFTstats = await getCollectionStats(nft.collection.slug);
 			setStats(NFTstats);
 		};
 		fetchAssets();
-	}, []);
+	}, [nft.collection]);
 
 	return (
 		<View style={{ flex: 1, backgroundColor: colors.background5 }}>
 			<SafeAreaView />
 
 			<View style={styles.topContainer}>
-				<Header title={nft.name} />
+				<Header title={nft?.name || ''} />
 				<Image
-					source={{ uri: nft.image }}
+					source={{ uri: nft?.image || '' }}
 					style={styles.image}
 				/>
-				<Text type="hMedium" weight="bold">{nft.name}</Text>
+				<Text type="hMedium" weight="bold">{nft?.name || ''}</Text>
 				<View style={styles.byContainer}>
 					<Text type="tSmall" weight="bold">
 						{i18n.t('NFTDetailScreen.by')}
 					</Text>
 					<Text type="tSmall" weight="bold" color="cta1">
-						{nft.collection.name}
+						{nft?.collection.name || ''}
 					</Text>
 				</View>
 				<NetworkWarning.Tag title={i18n.t('NFTDetailScreen.this_nft')} />
@@ -51,17 +52,17 @@ const NFTDetailScreen = ({ route }: Props) => {
 			<Bottom>
 				<Panel
 					floor={`$${stats?.floor_price ?? 0}`}
-					lastSale="$20.12"
+					lastSale={`${nft?.last_sale ? `$${truncate(nft?.last_sale.payment_token.usd_price, 2)}` : 'N/A'}`}
 				/>
 				<Button
 					iconRight="openInNew"
 					title={i18n.t('NFTDetailScreen.view_on_openSea')}
 					marginBottom={16}
-					onPress={() => Linking.openURL(nft.permalink)}
+					onPress={() => Linking.openURL(nft?.permalink || '')}
 				/>
 				<Expander
-					title={`${i18n.t('NFTDetailScreen.about')} ${nft.collection.name}`}
-					desc={nft.collection.desc}
+					title={`${i18n.t('NFTDetailScreen.about')} ${nft?.collection.name || ''}`}
+					desc={nft?.collection.desc || ''}
 				/>
 				<SafeAreaView />
 			</Bottom>
