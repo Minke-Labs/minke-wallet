bd87bf1ec3002470c91549c351b7cc2c0f40700f
Marcos Teixeira <m.viniteixeira@gmail.com>
2022-08-08 10:04:02 +0100

    Fix wrong flag after search

--- src/components/Flag/Flag.tsx
+++ src/components/Flag/Flag.tsx
@@ -29,10 +29,11 @@ const Flag: React.FC<Partial<FlagProps>> = ({ name = '', size = 96 }) => {
 		if (idx % 2 === 0) acc.push(arr.slice(idx, idx + 2));
 		return acc;
 	}, [] as any);
+
 	return (
 		<Svg width={size} height={size} viewBox="0 0 32 32" fill="none">
 			{newDArr.map((item: string, idx: number) => (
-				<Path key={`${item}-${idx}`} d={item[0]} fill={item[1]} />
+				<Path key={`${name}-${idx}`} d={item[0]} fill={item[1]} />
 			))}
 		</Svg>
 	);
