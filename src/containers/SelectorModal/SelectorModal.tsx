44211f153b293cfab90607cf68fc94d40d8cd7a5
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-08-08 18:55:17 +0900

    Contrast colors depending on theme

--- src/containers/SelectorModal/SelectorModal.tsx
+++ src/containers/SelectorModal/SelectorModal.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { useColorScheme } from 'react-native';
 import { Text, ListItem } from '@components';
 import { useLanguage } from '@hooks';
-import APay from '../APay.svg';
-import APayDark from '../APayDark.svg';
+import { os } from '@styles';
+import APay from '../APay';
+import GPay from '../GPay';
 
 interface SelectorModalProps {
 	onBuy: () => void;
@@ -12,19 +12,28 @@ interface SelectorModalProps {
 
 export const SelectorModal: React.FC<SelectorModalProps> = ({ onBuy, onExchange }) => {
 	const { i18n } = useLanguage();
-	const scheme = useColorScheme();
 	return (
 		<>
 			<Text marginBottom={16} weight="bold" type="hMedium">
 				{i18n.t('Containers.AddFunds.SelectorModal.add_funds')}
 			</Text>
-			<ListItem
-				tagType="svg"
-				SvgComponent={scheme === 'dark' ? APayDark : APay}
-				title={i18n.t('Containers.AddFunds.SelectorModal.buy_crypto')}
-				desc={i18n.t('Containers.AddFunds.SelectorModal.apple_card_transfer')}
-				onPress={onBuy}
-			/>
+			{os === 'android' ? (
+				<ListItem
+					tagType="svg"
+					SvgComponent={GPay}
+					title={i18n.t('Containers.AddFunds.SelectorModal.buy_crypto')}
+					desc={i18n.t('Containers.AddFunds.SelectorModal.apple_card_transfer', { pay: 'Google Pay' })}
+					onPress={onBuy}
+				/>
+			) : (
+				<ListItem
+					tagType="svg"
+					SvgComponent={APay}
+					title={i18n.t('Containers.AddFunds.SelectorModal.buy_crypto')}
+					desc={i18n.t('Containers.AddFunds.SelectorModal.apple_card_transfer', { pay: 'Apple Pay' })}
+					onPress={onBuy}
+				/>
+			)}
 			<ListItem
 				title={i18n.t('Containers.AddFunds.SelectorModal.external')}
 				desc={i18n.t('Containers.AddFunds.SelectorModal.send_from_exchange')}
