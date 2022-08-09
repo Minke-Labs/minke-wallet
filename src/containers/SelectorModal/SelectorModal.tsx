84820dea529d3b30c497823d8e19146608140b59
Marcos Teixeira <m.viniteixeira@gmail.com>
2022-08-09 11:31:16 +0100

    Fix Android visuals

--- src/containers/SelectorModal/SelectorModal.tsx
+++ src/containers/SelectorModal/SelectorModal.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useColorScheme } from 'react-native';
 import { Text, ListItem } from '@components';
 import { useLanguage } from '@hooks';
 import APay from '../APay.svg';
+import APayDark from '../APayDark.svg';
 
 interface SelectorModalProps {
 	onBuy: () => void;
@@ -10,6 +12,7 @@ interface SelectorModalProps {
 
 export const SelectorModal: React.FC<SelectorModalProps> = ({ onBuy, onExchange }) => {
 	const { i18n } = useLanguage();
+	const scheme = useColorScheme();
 	return (
 		<>
 			<Text marginBottom={16} weight="bold" type="hMedium">
@@ -17,7 +20,7 @@ export const SelectorModal: React.FC<SelectorModalProps> = ({ onBuy, onExchange 
 			</Text>
 			<ListItem
 				tagType="svg"
-				SvgComponent={APay}
+				SvgComponent={scheme === 'dark' ? APayDark : APay}
 				title={i18n.t('Containers.AddFunds.SelectorModal.buy_crypto')}
 				desc={i18n.t('Containers.AddFunds.SelectorModal.apple_card_transfer')}
 				onPress={onBuy}
