7ca4a1499385ad3b5a4b722c103f5b942b0aa198
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-08-17 06:42:44 +0900

    HomeScreen Header

--- src/screens/HomeScreen/Header/Header.tsx
+++ src/screens/HomeScreen/Header/Header.tsx
@@ -1,34 +1,73 @@
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
-import { View, Text, Icon } from '@components';
+import { GestureResponderEvent, TouchableOpacity } from 'react-native';
+import * as Haptics from 'expo-haptics';
+import { View, Text, Icon, ScreenLoadingIndicator } from '@components';
+import { useWalletState } from '@hooks';
 
-const Header = () => (
-	<View row main="space-between" cross="flex-end" mb={3}>
-		<View>
-			<Text type="lMedium" weight="semiBold">
-				Welcome
-			</Text>
-			<Text type="hMedium" weight="bold">
-				jreyes.eth
-			</Text>
+interface HeaderProps {
+	onPointsPress: () => void;
+	onSettingsPress: () => void;
+	onCopyPress: (event: GestureResponderEvent) => void;
+	points: number;
+}
+
+const Header: React.FC<HeaderProps> = ({ onPointsPress, onSettingsPress, onCopyPress, points }) => {
+	const { accountName, state } = useWalletState();
+
+	const handlePress = (event: any) => {
+		Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);
+		onCopyPress(event);
+	};
+
+	if (state.promised) return <ScreenLoadingIndicator />;
+
+	return (
+		<View row main="space-between" cross="flex-end" mb={3}>
+			<View>
+				<Text type="lMedium" weight="semiBold">
+					Welcome
+				</Text>
+
+				<TouchableOpacity activeOpacity={0.6} onPress={handlePress}>
+					<View row cross="center">
+						<Text type="hMedium" weight="bold">
+							{accountName}
+						</Text>
+						<View ml={3}>
+							<Icon name="copy" size={24} color="text7" />
+						</View>
+					</View>
+				</TouchableOpacity>
+
+			</View>
+			<View row cross="center">
+
+				<TouchableOpacity activeOpacity={0.6} onPress={onPointsPress}>
+					<View
+						br={2}
+						mr={2}
+						ph={2}
+						pv={1}
+						row
+						cross="center"
+						bg="background3"
+					>
+						<Text weight="semiBold" type="lSmall" color="cta1">
+							{points} points
+						</Text>
+					</View>
+				</TouchableOpacity>
+
+				<TouchableOpacity onPress={onSettingsPress}>
+					<Icon
+						name="gear"
+						size={28}
+						color="cta1"
+					/>
+				</TouchableOpacity>
+			</View>
 		</View>
-		<View row cross="center">
-			<View
-				w={72}
-				h={19}
-				br={2}
-				bg="alert3"
-				mr={3}
-			/>
-			<TouchableOpacity onPress={() => null}>
-				<Icon
-					name="gear"
-					size={28}
-					color="cta1"
-				/>
-			</TouchableOpacity>
-		</View>
-	</View>
-);
+	);
+};
 
 export default Header;
--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import { ScrollView, SafeAreaView } from 'react-native';
 import { BasicLayout } from '@layouts';
-import { View, Modal, ModalReusables } from '@components';
-import { useFormProgress } from '@hooks';
+import { Snackbar } from 'react-native-paper';
+import { View, Modal, ModalReusables, Text } from '@components';
+import { useFormProgress, useMinkeRewards, useNavigation, useWalletState } from '@hooks';
+import * as Clipboard from 'expo-clipboard';
+import * as Haptics from 'expo-haptics';
 import { Selector } from './Selector/Selector';
 import { Stories } from './Stories/Stories';
 import { Accounts } from './Accounts/Accounts';
@@ -14,13 +17,33 @@ const HomeScreen = () => {
 	const { currentStep, goBack, goForward } = useFormProgress();
 	const [visible, setVisible] = useState(false);
 	const [openAvatarModal, setOpenAvatarModal] = useState(false);
+	const [snackbarVisible, setSnackbarVisible] = useState(false);
+
+	const { state } = useWalletState();
+	const { address } = state.value;
+	const { points } = useMinkeRewards();
+	const navigation = useNavigation();
+
+	const onPointsPress = () => navigation.navigate('ReferralScreen');
+	const onSettingsPress = () => navigation.navigate('SettingsScreen');
+	const onCopyToClipboard = () => {
+		Clipboard.setString(address || '');
+		Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+		setSnackbarVisible(true);
+	};
+
 	return (
 		<>
 			<BasicLayout>
 				<SafeAreaView />
 				<ScrollView showsVerticalScrollIndicator={false}>
 					<View ph={3}>
-						<Header />
+						<Header
+							onPointsPress={onPointsPress}
+							onSettingsPress={onSettingsPress}
+							onCopyPress={onCopyToClipboard}
+							points={points}
+						/>
 						<Assets setOpenAvatarModal={() => setOpenAvatarModal(true)} />
 						<Accounts />
 						<Stories />
@@ -28,6 +51,7 @@ const HomeScreen = () => {
 				</ScrollView>
 				<Selector onActionPressed={() => setVisible(true)} />
 			</BasicLayout>
+
 			<Modal isVisible={visible} onDismiss={() => setVisible(false)}>
 				<ModalReusables.Actions />
 			</Modal>
@@ -42,6 +66,12 @@ const HomeScreen = () => {
 					onSelectAvatar={goForward}
 				/>
 			</Modal>
+
+			<Snackbar duration={2000} onDismiss={() => setSnackbarVisible(false)} visible={snackbarVisible}>
+				<Text color="text11">
+					Address copied
+				</Text>
+			</Snackbar>
 		</>
 	);
 };
