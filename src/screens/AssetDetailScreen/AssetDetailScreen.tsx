8637ac07fe2c36c8f5bdc5ce5b5d6015f60d7f4d
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-08-16 21:19:05 +0900

    By Networks screen

--- src/screens/AssetDetailScreen/AssetDetailScreen.tsx
+++ src/screens/AssetDetailScreen/AssetDetailScreen.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ScrollView } from 'react-native';
 import { View } from '@components';
 import { BasicLayout } from '@layouts';
 import { Header } from './Header/Header';
@@ -7,11 +8,13 @@ import ByNetworks from './ByNetworks/ByNetworks';
 
 const AssetDetailScreen = () => (
 	<BasicLayout>
-		<View ph={3}>
-			<Header onPress={() => null} />
-			<Balance />
-			<ByNetworks />
-		</View>
+		<ScrollView showsVerticalScrollIndicator={false}>
+			<View ph={3}>
+				<Header onPress={() => null} />
+				<Balance />
+				<ByNetworks />
+			</View>
+		</ScrollView>
 	</BasicLayout>
 );
 
