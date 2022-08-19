0f3652f213a9f61dcbe1bf9ae14f216eef770df1
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-08-19 22:43:01 +0900

    SVGs

--- src/screens/HomeScreen/Accounts/Accounts.tsx
+++ src/screens/HomeScreen/Accounts/Accounts.tsx
@@ -2,27 +2,67 @@ import React from 'react';
 import { Paper2, Text, View, Button } from '@components';
 import APay from './APay.svg';
 import Debit from './Debit';
+import { PayId, Interac, FasterPayments, Sepa } from './images';
 import Pix from './Pix.svg';
 
-export const Accounts = () => (
-	<Paper2 br="xs" p="xs" mb="xs">
-		<Text type="tSmall" weight="bold" color="cta1" mb="xs">
-			Buy USDC now!{'\n'}No personal ID required.
-		</Text>
-		<Text type="lSmall" weight="semiBold" mb="xs">
-			Purchase in a few clicks with:
-		</Text>
-		<View mb="s" row>
-			<APay />
-			<View mr="xs" />
-			<Debit />
-			<View mr="xs" />
-			<Pix />
-		</View>
-		<Button
-			title="Buy USDC now"
-			mode="outlined"
-			onPress={() => null}
-		/>
-	</Paper2>
-);
+const chooseLocation = (loc: string) => {
+	switch (loc) {
+		case 'AU':
+			return <PayId />;
+		case 'GB':
+			return <FasterPayments />;
+		case 'CA':
+			return <Interac />;
+		case 'EU':
+		case 'AT':
+		case 'BE':
+		case 'CY':
+		case 'EE':
+		case 'FI':
+		case 'FR':
+		case 'DE':
+		case 'GR':
+		case 'IE':
+		case 'IT':
+		case 'LV':
+		case 'LT':
+		case 'LU':
+		case 'MT':
+		case 'NE':
+		case 'PT':
+		case 'SK':
+		case 'SI':
+		case 'ES':
+			return <Sepa />;
+		case 'BR':
+			return <Pix />;
+		default:
+			return null;
+	}
+};
+
+export const Accounts = () => {
+	const localPayment = chooseLocation('BR');
+	return (
+		<Paper2 br="xs" p="xs" mb="xs">
+			<Text type="tSmall" weight="bold" color="cta1" mb="xs">
+				Buy USDC now!{'\n'}No personal ID required.
+			</Text>
+			<Text type="lSmall" weight="semiBold" mb="xs">
+				Purchase in a few clicks with:
+			</Text>
+			<View mb="s" row cross="center">
+				<APay />
+				<View mr="xs" />
+				<Debit />
+				<View mr="xs" />
+				{localPayment}
+			</View>
+			<Button
+				title="Buy USDC now"
+				mode="outlined"
+				onPress={() => null}
+			/>
+		</Paper2>
+	);
+};
---
+++ src/screens/HomeScreen/Accounts/images/index.ts
@@ -0,0 +1,11 @@
+import FasterPayments from './FasterPayments';
+import Interac from './Interac';
+import PayId from './PayId';
+import Sepa from './Sepa';
+
+export {
+	FasterPayments,
+	Interac,
+	PayId,
+	Sepa
+};
