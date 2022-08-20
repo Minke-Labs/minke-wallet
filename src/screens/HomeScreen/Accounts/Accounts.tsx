b14e7fe60c429cc56bbe519a5098e74f8fcce9b1
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-08-20 13:13:43 +0900

    AddFunds modal on Accounts

--- src/screens/HomeScreen/Accounts/Accounts.tsx
+++ src/screens/HomeScreen/Accounts/Accounts.tsx
@@ -1,34 +1,41 @@
-import React from 'react';
-import { Paper2, Text, View, Button } from '@components';
+import React, { useState } from 'react';
+import { Paper2, Text, View, Button, ModalBase } from '@components';
 import { useCountry } from '@hooks';
+import { AddFunds } from '@containers';
 import APay from './APay.svg';
 import Debit from './Debit';
 import { chooseLocation } from './chooseLocation';
 
 export const Accounts = () => {
+	const [addFundsVisible, setAddFundsVisible] = useState(false);
 	const { countryCode } = useCountry();
 	const localPayment = chooseLocation(countryCode);
 
 	return (
-		<Paper2 br="xs" p="xs" mb="xs">
-			<Text type="tSmall" weight="bold" color="cta1" mb="xs">
-				Buy USDC now!{'\n'}No personal ID required.
-			</Text>
-			<Text type="lSmall" weight="semiBold" mb="xs">
-				Purchase in a few clicks with:
-			</Text>
-			<View mb="s" row cross="center">
-				<APay />
-				<View mr="xs" />
-				<Debit />
-				<View mr="xs" />
-				{localPayment}
-			</View>
-			<Button
-				title="Buy USDC now"
-				mode="outlined"
-				onPress={() => null}
-			/>
-		</Paper2>
+		<>
+			<Paper2 br="xs" p="xs" mb="xs">
+				<Text type="tSmall" weight="bold" color="cta1" mb="xs">
+					Buy USDC now!{'\n'}No personal ID required.
+				</Text>
+				<Text type="lSmall" weight="semiBold" mb="xs">
+					Purchase in a few clicks with:
+				</Text>
+				<View mb="s" row cross="center">
+					<APay />
+					<View mr="xs" />
+					<Debit />
+					<View mr="xs" />
+					{localPayment}
+				</View>
+				<Button
+					title="Buy USDC now"
+					mode="outlined"
+					onPress={() => setAddFundsVisible(true)}
+				/>
+			</Paper2>
+			<ModalBase isVisible={addFundsVisible} onDismiss={() => setAddFundsVisible(false)}>
+				<AddFunds visible={addFundsVisible} onDismiss={() => setAddFundsVisible(false)} />
+			</ModalBase>
+		</>
 	);
 };
