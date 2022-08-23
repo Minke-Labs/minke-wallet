9ed67da6c8b1d71c4a9fd5ff7038a6b935aee7c2
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-08-23 22:08:37 +0900

    IconItem refactored

--- src/components/IconItem/IconItem.tsx
+++ src/components/IconItem/IconItem.tsx
@@ -4,29 +4,42 @@ import { SpacingType, IconType } from '@styles';
 import IconBox from '../IconBox/IconBox';
 import Text from '../Text/Text';
 import View from '../View/View';
+import Icon from '../Icon/Icon';
 
 interface IconItemProps {
 	mb?: SpacingType;
 	title: string;
-	desc: string;
+	desc?: string;
 	icon: IconType;
 	alert?: boolean;
+	rightButton?: boolean;
 	onPress: () => void;
 }
 
-const IconItem: React.FC<IconItemProps> = ({ mb, title, desc, icon, alert, onPress }) => (
+const IconItem: React.FC<IconItemProps> = ({ mb, title, desc, icon, alert, rightButton, onPress }) => (
 	<View mb={mb}>
 		<TouchableOpacity onPress={onPress}>
-			<View row>
-				<IconBox icon={icon} bgc="background2" alert={alert} />
-				<View>
-					<Text type="lLarge" weight="semiBold" color={alert ? 'alert1' : 'text1'}>
-						{title}
-					</Text>
-					<Text type="bSmall" color={alert ? 'alert1' : 'text4'}>
-						{desc}
-					</Text>
+			<View row main="space-between" cross="center">
+				<View row cross="center">
+					<IconBox icon={icon} bgc="background2" alert={alert} />
+					<View>
+						<Text type="lLarge" weight="semiBold" color={alert ? 'alert1' : 'text1'}>
+							{title}
+						</Text>
+						{!!desc && (
+							<Text type="bSmall" color={alert ? 'alert1' : 'text4'}>
+								{desc}
+							</Text>
+						)}
+					</View>
 				</View>
+				{rightButton && (
+					<Icon
+						name="chevronRight"
+						size={24}
+						color={alert ? 'alert1' : 'cta1'}
+					/>
+				)}
 			</View>
 		</TouchableOpacity>
 	</View>
