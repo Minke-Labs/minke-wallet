ad52b4faf5f41381c07feea5cd51b2364ef3f11e
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-08-24 22:09:33 +0900

    Expander refactored

--- src/components/Expander/Expander.tsx
+++ src/components/Expander/Expander.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import { View, TouchableOpacity } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 import { useLanguage } from '@hooks';
 import Text from '../Text/Text';
 import Paper from '../Paper/Paper';
 import Icon from '../Icon/Icon';
+import View from '../View/View';
 import { ExpanderProps } from './Expander.types';
-import styles from './Expander.styles';
 
 const lengthConst = 181;
 
@@ -19,42 +19,32 @@ const Expander: React.FC<ExpanderProps> = ({ title, desc }) => {
 	};
 
 	return (
-		<Paper p="xs" style={{ width: '100%' }}>
-			<Text
-				type="tSmall"
-				weight="semiBold"
-				marginBottom={8}
-			>
+		<Paper p="xs">
+			<Text type="tSmall" weight="semiBold" mb="xxs">
 				{title}
 			</Text>
 			<Text type="bSmall" color="text4">
 				{trunc()}
 			</Text>
-			{
-				desc.length > lengthConst && (
-					<View style={styles.expand}>
-						<TouchableOpacity
-							style={styles.button}
-							onPress={() => setExpanded(!expanded)}
-						>
-							<Text
-								type="lSmall"
-								weight="semiBold"
-								style={{ marginRight: 8 }}
-							>
+			{desc.length > lengthConst && (
+				<View h={38} row main="flex-end" cross="center">
+					<TouchableOpacity onPress={() => setExpanded(!expanded)}>
+						<View row cross="center">
+							<Text type="lSmall" weight="semiBold">
 								{expanded ?
 									i18n.t('NFTDetailScreen.Expander.show_less') :
 									i18n.t('NFTDetailScreen.Expander.show_more')}
 							</Text>
+							<View mr="xxxs" />
 							<Icon
 								name={expanded ? 'chevronUp' : 'chevronDown'}
 								size={20}
 								color="cta1"
 							/>
-						</TouchableOpacity>
-					</View>
-				)
-			}
+						</View>
+					</TouchableOpacity>
+				</View>
+			)}
 		</Paper>
 	);
 };
