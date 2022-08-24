de30ea88c45dc915475c7777a38f0508654721b4
Marcos Teixeira <m.viniteixeira@gmail.com>
2022-08-24 12:53:58 +0100

    Start investments screen

--- src/components/TokenItem/TokenItem.tsx
+++ src/components/TokenItem/TokenItem.tsx
@@ -16,54 +16,57 @@ interface TokenItemProps {
 	perc?: number;
 }
 
-const TokenItem: React.FC<TokenItemProps> = ({ token, name, symbol, subtitle, balance, balanceUSD, perc }) => (
-	<View row main="space-between">
-		<View row>
-			<Token name={token} size={39} />
-			<View mr="xxs" />
-			<View>
-				<View row cross="center">
-					<Text type="lLarge" weight="semiBold">
-						{name || symbol}
-					</Text>
-					<View mr="xxs" />
-					<Text type="bSmall" color="text3">
-						{name ? symbol : ''}
+const TokenItem: React.FC<TokenItemProps> = ({ token, name, symbol, subtitle, balance, balanceUSD, perc }) => {
+	const tokenName = name || symbol;
+	return (
+		<View row main="space-between">
+			<View row>
+				<Token name={token} size={39} />
+				<View mr="xxs" />
+				<View>
+					<View row cross="center">
+						<Text type="lLarge" weight="semiBold">
+							{tokenName.length < 20 ? tokenName : `${tokenName.substring(0, 17)}...`}
+						</Text>
+						<View mr="xxs" />
+						<Text type="bSmall" color="text3">
+							{name ? symbol : ''}
+						</Text>
+					</View>
+					<Text type="lSmall" weight="semiBold">
+						{subtitle}
 					</Text>
 				</View>
-				<Text type="lSmall" weight="semiBold">
-					{subtitle}
-				</Text>
+			</View>
+
+			{!!perc && <Tag perc={perc} />}
+
+			<View main="center">
+				{!!balance && !!balanceUSD && (
+					<View cross="flex-end">
+						<>
+							<Text type="lLarge" weight="semiBold">
+								{tokenBalanceFormat(balance, 4)}
+							</Text>
+							<Text type="bDetail" color="text3">
+								{numberFormat(balanceUSD)}
+							</Text>
+						</>
+					</View>
+				)}
+				{!!balance && !balanceUSD && (
+					<Text type="lLarge" weight="semiBold">
+						{tokenBalanceFormat(balance, 4)}
+					</Text>
+				)}
+				{!!balanceUSD && !balance && (
+					<Text type="lLarge" weight="semiBold">
+						{numberFormat(balanceUSD)}
+					</Text>
+				)}
 			</View>
 		</View>
-
-		{!!perc && <Tag perc={perc} />}
-
-		<View main="center">
-			{!!balance && !!balanceUSD && (
-				<View cross="flex-end">
-					<>
-						<Text type="lLarge" weight="semiBold">
-							{tokenBalanceFormat(balance, 4)}
-						</Text>
-						<Text type="bDetail" color="text3">
-							{numberFormat(balanceUSD)}
-						</Text>
-					</>
-				</View>
-			)}
-			{!!balance && !balanceUSD && (
-				<Text type="lLarge" weight="semiBold">
-					{tokenBalanceFormat(balance, 4)}
-				</Text>
-			)}
-			{!!balanceUSD && !balance && (
-				<Text type="lLarge" weight="semiBold">
-					{numberFormat(balanceUSD)}
-				</Text>
-			)}
-		</View>
-	</View>
-);
+	);
+};
 
 export default TokenItem;
--- src/screens/InvestmentsScreen/InvestmentsScreen.tsx
+++ src/screens/InvestmentsScreen/InvestmentsScreen.tsx
@@ -1,24 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { ScrollView } from 'react-native';
 import { Text, View, TokenItemCard, BlankStates } from '@components';
 import { AssetsLayout } from '@layouts';
 import { useNavigation, useTokens } from '@hooks';
 import { TokenType } from '@styles';
-import { numberFormat } from '@helpers/utilities';
 import { MinkeToken } from '@models/types/token.types';
-import Selector from './Selector/Selector';
+// import Selector from './Selector/Selector';
 
 const InvestmentsScreen = () => {
 	const { tokens, walletBalance: balance } = useTokens();
 
-	const [active, setActive] = useState(0);
+	// const [active, setActive] = useState(0);
 	const navigation = useNavigation();
 
-	if (tokens === undefined || tokens?.length === 0) return <BlankStates.WalletAssets />;
+	if (tokens === undefined || tokens?.length === 0) return <BlankStates.WalletAssets />; // @TODO: Fix skeleton title
 
 	return (
 		<AssetsLayout
-			headerValue={numberFormat(balance || 0)}
+			headerValue={balance}
 			headerTitle={
 				<Text type="lLarge" weight="semiBold" color="text3">
 					Current value
@@ -31,7 +30,7 @@ const InvestmentsScreen = () => {
 						Investments
 					</Text>
 
-					<Selector {...{ active, setActive }} />
+					{/* <Selector {...{ active, setActive }} /> */}
 
 					<View pr="xs" mt="xs">
 						{tokens.map((item: MinkeToken) => (
@@ -43,7 +42,6 @@ const InvestmentsScreen = () => {
 								subtitle="All networks"
 								balance={item.balance}
 								balanceUSD={item.balanceUSD}
-								// perc={12.40}
 								onPress={() => navigation.navigate('AssetDetailScreen', { coin: item })}
 							/>
 						))}
