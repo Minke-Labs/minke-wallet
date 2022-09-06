61ae2b33c5c6c830d707a3cd662a0e021e7a4240
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-09-06 19:55:40 +0900

    fix - bug of modals not clickable

--- src/components/Touchable/Touchable.tsx
+++ src/components/Touchable/Touchable.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { TouchableOpacity } from 'react-native';
 import { ViewType } from '@styles';
 import View from '../View/View';
 
