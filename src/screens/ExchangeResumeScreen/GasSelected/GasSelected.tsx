c9889240a238be7fb92e36c610612b8a90cf03ae
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-09-09 18:52:26 +0900

    refactoring - ExchangeResumeScreen

--- src/screens/ExchangeResumeScreen/GasSelected/GasSelected.tsx
+++ src/screens/ExchangeResumeScreen/GasSelected/GasSelected.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useCallback } from 'react';
-import { View } from 'react-native';
-import { Paper, GasOptionInner } from '@components';
+import { Paper, GasOptionInner, View } from '@components';
 import { State, useState } from '@hookstate/core';
 import { estimateGas, getEthLastPrice, estimateConfirmationTime } from '@models/wallet';
 import { ExchangeState, globalExchangeState, Gas } from '@stores/ExchangeStore';
@@ -102,15 +101,13 @@ export const GasSelected = () => {
 	}, [gasPrice]);
 
 	return (
-		<Paper p="xs" m="xs">
-			<View style={{ flexDirection: 'row', alignItems: 'center' }}>
-				<GasOptionInner
-					type={selectedType!}
-					gasPrice={gasPrice!}
-					usdPrice={usdPrice!}
-					waiting={waiting()}
-				/>
-			</View>
+		<Paper row cross="center" p="xs" m="xs" mh="xs">
+			<GasOptionInner
+				type={selectedType!}
+				gasPrice={gasPrice!}
+				usdPrice={usdPrice!}
+				waiting={waiting()}
+			/>
 		</Paper>
 	);
 };
