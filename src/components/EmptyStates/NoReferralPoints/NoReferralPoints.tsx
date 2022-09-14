fed7beecaebb4e18a32a7e5088657b475b9e8412
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-09-14 15:04:59 +0900

    Send to referral

--- src/components/EmptyStates/NoReferralPoints/NoReferralPoints.tsx
+++ src/components/EmptyStates/NoReferralPoints/NoReferralPoints.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import { Image, View } from 'react-native';
+import { Image } from 'react-native';
 import { whale2Img } from '@images';
-import { useLanguage } from '@hooks';
-import Button from '../../Button/Button';
-import Text from '../../Text/Text';
-import { styles } from './NoReferralPoints.styles';
+import { useLanguage, useNavigation } from '@hooks';
+import Button from '@src/components/Button/Button';
+import Text from '@src/components/Text/Text';
+import View from '@src/components/View/View';
 
 const NoReferralPoints = ({ onEarnPress }: { onEarnPress: () => void }) => {
 	const { i18n } = useLanguage();
+	const navigation = useNavigation();
 	return (
-		<View style={styles.container}>
-			<Image source={whale2Img} style={styles.image} />
-			<Text type="p2" color="text1" weight="bold" mb="xxs">
+		<View cross="center" pt="m">
+			<Image
+				source={whale2Img}
+				style={{ width: 152, height: 152 }}
+			/>
+			<Text type="tSmall" weight="bold" mb="xxs">
 				{i18n.t('Components.EmptyStates.NoReferralPoints.your_points_will_appear_here')}
 			</Text>
-			<Text type="p2" color="text1" weight="regular" mb="s">
+			<Text type="bMedium" mb="l">
 				{i18n.t('Components.EmptyStates.NoReferralPoints.lets_get_started')}
 			</Text>
 			<Button
 				title={i18n.t('Components.EmptyStates.NoReferralPoints.earn_points')}
 				iconLeft="add"
 				onPress={onEarnPress}
+			/>
+			<View mb="xxxs" />
+			<Button
+				title="I have a referral code"
+				mode="empty"
+				onPress={() => navigation.navigate('EnterReferralCodeScreen')}
 			/>
 		</View>
 	);
