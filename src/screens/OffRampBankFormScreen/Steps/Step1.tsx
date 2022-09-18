8ecb253071fc1f7dfedde5ef86a141f069978f8c
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-09-18 09:37:28 +0900

    temp

--- src/screens/OffRampBankFormScreen/Steps/Step1.tsx
+++ src/screens/OffRampBankFormScreen/Steps/Step1.tsx
@@ -1,17 +1,59 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-tabs */
 import React, { useState } from 'react';
 import { TelephoneInput, Text, Input, ChangeCountry } from '@components';
 import { AreaObjType } from '@src/components/TelephoneInput/TelephoneInput.types';
 
-// type Iso = 'US' | 'BR' | 'AU';
+const regName = /^[a-z ,.'-]+$/i;
+
+type Erros = {
+	firstName: string;
+};
 
 const Step1: React.FC = () => {
-	const [modalVisible, setModalVisible] = useState(false);
 	const [iso, setIso] = useState<AreaObjType>('US');
 	const [telephone, setTelephone] = useState('');
-	const [firstName, setFirstName] = useState('');
-	const [firstNameError, setFirstNameError] = useState(true);
-	const [lastName, setLastName] = useState('');
+
+	const [inputs, setInputs] = useState({
+		firstname: ''
+	});
+	const [errors, setErrors] = useState<Erros>({} as Erros);
+
+	const handleOnchange = (text: string, input: string) => {
+		setInputs((prevState) => ({ ...prevState, [input]: text }));
+	};
+
+	const handleError = (error: string, input: string) => {
+		setErrors((prevState) => ({ ...prevState, [input]: error }));
+	};
+
+	// const [form, setForm] = useState({
+	// 	firstName: '',
+	// 	lastName: ''
+	// });
+
+	// const [errors, setErrors] = useState({
+	// 	firstName: {
+	// 		error: false,
+	// 		message: 'Invalid characters. Use only (A-Z)'
+	// 	},
+	// 	lastName: {
+	// 		error: false,
+	// 		message: 'Invalid characters. Use only (A-Z)'
+	// 	}
+	// });
+
+	// const handleUpdate = (label: string, txt: string) => {
+	// 	const formObj = {
+	// 		...form,
+	// 		[label]: {
+	// 			...[label],
+	// 			label: txt,
+	// 			error: !regName.test(txt)
+	// 		}
+	// 	};
+	// 	setForm(formObj);
+	// };
 
 	return (
 		<>
@@ -22,38 +64,40 @@ const Step1: React.FC = () => {
 			</Text>
 
 			<Input
-				onChangeText={(val) => setFirstName(val)}
-				value={firstName}
+				onChangeText={(val) => handleUpdate('firstName', val)}
+				value={form.firstName}
 				label="First name"
-				mb={firstNameError ? 'xxs' : 's'}
-				error={firstNameError}
+				errorDesc={errors.firstName}
+				// mb={errors.firstName.error ? 'xxs' : 's'}
+			/>
+
+			{/* <Input
+				onChangeText={(val) => handleUpdate('firstName', val)}
+				value={form.firstName}
+				label="First name"
+				mb={errors.firstName.error ? 'xxs' : 's'}
+				error={errors.firstName.error}
 				errorDesc="Invalid characters. Use only (A-Z)"
-			/>
+			/> */}
 
-			<Input
-				onChangeText={(val) => setLastName(val)}
-				value={lastName}
+			{/* <Input
+				onChangeText={(val) => handleUpdate('lastName', val)}
+				value={form.lastName}
 				label="Last name"
-				mb="s"
-			/>
+				mb={errors.lastName.error ? 'xxs' : 's'}
+				error={errors.lastName.error}
+				errorDesc="Invalid characters. Use only (A-Z)"
+			/> */}
 
-			<Input
+			{/* <Input
 				label="Birthday"
 				mb="s"
-			/>
+			/> */}
 
-			<TelephoneInput.Input
+			<TelephoneInput
 				label="Mobile number"
 				value={telephone}
 				onChangeText={(t) => setTelephone(t)}
-				openModal={() => setModalVisible(true)}
-				iso={iso}
-			/>
-
-			<TelephoneInput.Modal
-				setValue={(val: AreaObjType) => setIso(val)}
-				isVisible={modalVisible}
-				onDismiss={() => setModalVisible(false)}
 			/>
 		</>
 	);
