722c8b3184f78fe754da5bd5736b40438b246a23
Marcos Teixeira <m.viniteixeira@gmail.com>
2023-05-16 11:25:18 +0100

    Remove add funds button

--- src/containers/SelectorModal/SelectorModal.tsx
+++ src/containers/SelectorModal/SelectorModal.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Text, ListItem } from '@components';
+
+import { ListItem, Text, View } from '@components';
 import { useLanguage } from '@hooks';
 import { os } from '@styles';
+
 import APay from '../APay';
 import GPay from '../GPay';
 
@@ -17,23 +19,25 @@ export const SelectorModal: React.FC<SelectorModalProps> = ({ onBuy, onExchange 
 			<Text mb="s" weight="bold" type="hMedium">
 				{i18n.t('Containers.AddFunds.SelectorModal.add_funds')}
 			</Text>
-			{os === 'android' ? (
-				<ListItem
-					tagType="svg"
-					SvgComponent={GPay}
-					title={i18n.t('Containers.AddFunds.SelectorModal.buy_crypto')}
-					desc={i18n.t('Containers.AddFunds.SelectorModal.apple_card_transfer', { pay: 'Google Pay' })}
-					onPress={onBuy}
-				/>
-			) : (
-				<ListItem
-					tagType="svg"
-					SvgComponent={APay}
-					title={i18n.t('Containers.AddFunds.SelectorModal.buy_crypto')}
-					desc={i18n.t('Containers.AddFunds.SelectorModal.apple_card_transfer', { pay: 'Apple Pay' })}
-					onPress={onBuy}
-				/>
-			)}
+			<View style={{ display: 'none' }}>
+				{os === 'android' ? (
+					<ListItem
+						tagType="svg"
+						SvgComponent={GPay}
+						title={i18n.t('Containers.AddFunds.SelectorModal.buy_crypto')}
+						desc={i18n.t('Containers.AddFunds.SelectorModal.apple_card_transfer', { pay: 'Google Pay' })}
+						onPress={onBuy}
+					/>
+				) : (
+					<ListItem
+						tagType="svg"
+						SvgComponent={APay}
+						title={i18n.t('Containers.AddFunds.SelectorModal.buy_crypto')}
+						desc={i18n.t('Containers.AddFunds.SelectorModal.apple_card_transfer', { pay: 'Apple Pay' })}
+						onPress={onBuy}
+					/>
+				)}
+			</View>
 			<ListItem
 				title={i18n.t('Containers.AddFunds.SelectorModal.external')}
 				desc={i18n.t('Containers.AddFunds.SelectorModal.send_from_exchange')}
