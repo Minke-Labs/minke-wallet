827fc0880d60e507a89995625ec07e365a45e02f
Marcos Teixeira <m.viniteixeira@gmail.com>
2023-11-13 16:05:55 +0300

    Bump version

--- src/hooks/useWalletEnabled.ts
+++ src/hooks/useWalletEnabled.ts
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react';
 import { disabledWallet } from '@src/services/apis/minke/minke';
+import { Platform } from 'react-native';
 
 const useWalletEnabled = () => {
 	const [walletEnabled, setWalletEnabled] = useState<boolean | undefined>();
 
 	const initializeServices = async () => {
-		if (!__DEV__) {
+		if (!__DEV__ || Platform.OS === 'android') {
 			const { enabled } = await disabledWallet({ wallet: '' });
 			setWalletEnabled(enabled);
 		} else {
